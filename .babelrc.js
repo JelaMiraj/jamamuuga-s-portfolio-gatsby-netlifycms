0802489dc5704fc63f59e2323ead39e4e18b4b3e
Jason Cady <jamamuuga@gmail.com>
2018-11-12 17:30:01 -0800

    Add babel config.

---
+++ .babelrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    "@babel/preset-react",
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    // "babel-plugin-add-module-exports",
+    "babel-plugin-styled-components",
+  ],
+}
