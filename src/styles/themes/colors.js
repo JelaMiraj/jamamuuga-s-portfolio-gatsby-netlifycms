09fec915dfc9135518123ef050c72ee4e3b3c81b
Jason Cady <jamamuuga@gmail.com>
2019-01-18 13:08:13 -0800

    Added theme colors file.

---
+++ src/styles/themes/colors.js
@@ -0,0 +1,18 @@
+// Config vars
+
+// TODO: Port this to Rebass maybe using styled-system and/or styled-components-theme
+
+// Kaldi logo brand color: #FD461E
+
+const kaldiRed = '#D64000'
+const kaldiRedInvert = 'white'
+export const colors = {
+  kaldiRed: kaldiRed,
+  kaldiRedInvert: kaldiRedInvert,
+  primary: kaldiRed,
+  primaryInvert: kaldiRedInvert,
+  background: kaldiRedInvert,
+  bodyColor: '#333',
+}
+
+export default colors
