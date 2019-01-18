016b342f6ad9df44b4b68bcb5e2765c2a4cc10db
Jason Cady <jamamuuga@gmail.com>
2019-01-17 19:08:00 -0800

    Backed up babel config suggested by eslint-with-prettier.

---
+++ .babelrc.eslint-with-prettier.backup.js
@@ -0,0 +1,44 @@
+{
+  // An important note is that babelrc does not merge presets or plugins
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: '> 0.25%, not dead',
+      },
+    ],
+    '@babel/preset-react',
+    // '@babel/preset-flow',
+  ],
+  plugins: [
+    // 'babel-plugin-emotion', // yarn add emotion react-emotion && yarn add babel-plugin-emotion --dev --exact
+    // ['babel-plugin-styled-components', { displayName: true }] // yarn add styled-components && yarn add babel-plugin-emotion --dev --exact
+  ],
+  env: {
+    production: {
+      plugins: [
+        'lodash',
+      ],
+    },
+    development: {
+      plugins: [
+        'react-hot-loader/babel',
+      ],
+    },
+    test: {
+      presets: [
+        [
+          '@babel/env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@babel/preset-react',
+        // '@babel/preset-flow',
+      ],
+      compact: false,
+    },
+  },
+}
