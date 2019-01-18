0884f43a7e7955cad6392f4fd423721287deee6d
Jason Cady <jamamuuga@gmail.com>
2019-01-17 21:40:55 -0800

    Split styles into scss module partials.
    Git handled this a bit oddly but it works out ok. Git assumed all.scss was renamed to _helpers.module.scss which is really has one of the larger chunks.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
 
 import Navbar from '../components/Navbar'
-import './all.scss'
+import '../styles/main.module.scss'
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
