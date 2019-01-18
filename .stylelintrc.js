a259c6aa2473312b08f3f1740705fe47b731de61
Jason Cady <jamamuuga@gmail.com>
2019-01-17 19:48:11 -0800

    Updated Stylelint config to match project's use of css.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
+  processors: [
+    'stylelint-processor-styled-components',
+  ],
   extends: [
     // 'stylelint-config-recommended-scss',
-    // 'stylelint-config-standard',
-    // 'stylelint-config-prettier',
+    'stylelint-config-standard-scss',
+    'stylelint-config-prettier',
+    'stylelint-config-styled-components',
+    // '@acollier/stylelint-config-scss',
+    // 'stylelint-config-airbnb',
+    // 'stylelint-config-idiomatic-sass',
+    // 'stylelint-config-pack',
   ],
 }
