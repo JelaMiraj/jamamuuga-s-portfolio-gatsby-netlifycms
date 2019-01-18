4125a89f5b27b20344002e8aaf8d080219c19d23
Jason Cady <jamamuuga@gmail.com>
2019-01-18 12:08:03 -0800

    Added Web Manifest config for Gatsby plugin manifest/

---
+++ .manifest.config.js
@@ -0,0 +1,13 @@
+import { colors } from './src/styles/themes/colors'
+
+export const manifestConfig = {
+  name: 'Jason Cady Portfolio',
+  short_name: 'Jason Cady Portfolio',
+  start_url: '/',
+  background_color: colors.background,
+  theme_color: colors.primary,
+  display: 'browser',
+  icon: 'src/img/logo.svg',
+}
+
+export default manifestConfig
