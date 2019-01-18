b263b72e2373290b0f1b600128855403e201fe0e
Jason Cady <jamamuuga@gmail.com>
2019-01-18 12:07:17 -0800

    Unified quote style in Babel config.

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
-    "@babel/preset-react",
+    '@babel/preset-react',
     'babel-preset-gatsby',
   ],
   plugins: [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-syntax-dynamic-import",
-    // "babel-plugin-add-module-exports",
-    "babel-plugin-styled-components",
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    // 'babel-plugin-add-module-exports',
+    'babel-plugin-styled-components',
   ],
 }
