93d84467dccba8eb44908894b03706a65306ad45
Jason Cady <jamamuuga@gmail.com>
2019-01-21 11:22:24 -0800

    Added Remark Lint config.

---
+++ .remarkrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    'preset-lint-recommended',
+    'preset-lint-consistent',
+    'preset-lint-markdown-style-guide',
+  ],
+  settings: {
+    commonmark: true,
+  },
+}
