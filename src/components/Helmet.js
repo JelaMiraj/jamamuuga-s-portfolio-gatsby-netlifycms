bc1c406f4784c29cb555554c995f3841a9777032
Jason Cady <jamamuuga@gmail.com>
2019-01-22 14:22:25 -0800

    More preparation of external header.

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactHelmet from 'react-helmet'
-// import { StaticQuery, graphql } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby'
 import {withTheme} from 'styled-components'
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 // TODO: Actually port the header over.
 // const Helmet = ({Theme = {}})
-const Helmet = null
+const Helmet = ({ theme = {} }) => (
 
-export default Helmet
+)
+
+Helmet.PropTypes = {
+  theme: PropTypes.object,
+}
+
+export default withTheme(Helmet)
