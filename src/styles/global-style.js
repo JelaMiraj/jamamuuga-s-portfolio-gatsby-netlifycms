cb31c2b6c37734c855849d01e1a88d516bf7070a
Jason Cady <jamamuuga@gmail.com>
2019-01-22 14:23:19 -0800

    Preparing external GlobalStyle including Styled Reboot

--- src/styles/global-style.js
+++ src/styles/global-style.js
@@ -1,7 +1,15 @@
 import styled, {createGlobalStyle} from 'styled-components'
+import reboot from 'styled-reboot'
+// TODO: Import a proper Styled System theme here instead of a separate pure color object.
+import colors from '../styles/themes/colors'
 
-const globalStyle = createGlobalStyle`
+const options = {}
+
+const rebootCss = reboot(/* options */)
+
+const GlobalStyle = createGlobalStyle`
+  ${rebootCss}
 
 `
 
-export default globalStyle
+export default GlobalStyle
