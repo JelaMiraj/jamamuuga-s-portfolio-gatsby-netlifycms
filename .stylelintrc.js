8fef3da08167f62727481558cae256650e54e700
Jason Cady <jamamuuga@gmail.com>
2019-02-01 22:52:40 -0800

    Added security warning about commented & removed stylelint-config-airbnb.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     'stylelint-config-prettier',
     'stylelint-config-styled-components',
     // '@acollier/stylelint-config-scss',
+    // NOTE: stylelint-config-airbnb removed because of unfixed security advisories of dependencies.
     // 'stylelint-config-airbnb',
     // 'stylelint-config-idiomatic-sass',
     // 'stylelint-config-pack',
