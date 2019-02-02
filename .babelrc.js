3b520cb0f7e410a55d8b7aa1e067cdedad0edd5a
Jason Cady <jamamuuga@gmail.com>
2019-02-01 18:20:44 -0800

    Explicitly enable Babel Preset Jest.

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', 'babel-preset-gatsby'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    'jest',
+    'babel-preset-gatsby',
+  ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import',
