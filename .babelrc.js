40d60806af6ce14ce297460401dfc3e6cf49f6ca
Jason Cady <jamamuuga@gmail.com>
2019-02-01 18:59:25 -0800

    Switch back to implicit Babel-Preset-Jest.

--- .babelrc.js
+++ .babelrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    'jest',
-    'babel-preset-gatsby',
-  ],
+  presets: ['@babel/preset-env', '@babel/preset-react', 'babel-preset-gatsby'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import',
