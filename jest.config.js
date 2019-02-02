6964bdc4b331685332f33fa2ba1fbaca8437a93f
Jason Cady <jamamuuga@gmail.com>
2019-02-01 17:35:50 -0800

    Reorganize Jest scripts & integrate Jest stuff from gatsby-storybook-jest-starter

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,33 @@
 module.exports = {
-  setupTestFrameworkScriptFile: 'jest-enzyme',
+  // collectCoverageFrom: [
+  //   'src/**/*.js',
+  // ],
+  // globals: {
+  //   __PATH_PREFIX__: '',
+  // },
+  // moduleNameMapper: {
+  //   '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+  //   '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  // },
+  setupFiles: [
+    // '<rootDir>/scripts/loadershim.js',
+    // 'raf/polyfill',
+  ],
+  setupFilesAfterEnv: [
+    // '<rootDir>/scripts/setupTests.js',
+    // '<rootDir>/scripts/jest.setup.js',
+    'jest-enzyme',
+  ],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
   testEnvironment: 'enzyme',
-  // snapshotSerializers: ['enzyme-to-json/serializer'],
-  // setupFiles: ['raf/polyfill', '<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: [
+    'node_modules',
+    '/.cache/',
+  ],
+  // transform: {
+  //   '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
+  // },
+  transformIgnorePatterns: [
+    'node_modules/(?!(gatsby)/)',
+  ],
 }
