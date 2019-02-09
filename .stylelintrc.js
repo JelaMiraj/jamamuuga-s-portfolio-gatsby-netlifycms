cb975482dcf90ae26dae4c023bedf466f2cdf7a8
Jason Cady <jamamuuga@gmail.com>
2019-02-08 17:12:43 -0800

    Added commented out stylelint-config-recommended in Stylelint config, in case it turns out to be a better match than similar ones.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   processors: ['stylelint-processor-styled-components'],
   extends: [
+    // TODO: Maybe use regular recommended with Styled Components.
+    // 'stylelint-config-recommended',
     // 'stylelint-config-recommended-scss',
     'stylelint-config-standard-scss',
     'stylelint-config-prettier',
