239f89b7e8e35e39e8196cb38f7839103af16058
Jason Cady <jamamuuga@gmail.com>
2019-02-08 17:10:39 -0800

    Added commented out rule, in Eslint config, to allow tagged templates, in case Eslint doesn't like them.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,8 @@ module.exports = {
   },
   extends: ['eslint-config-with-prettier', 'eslint-config-gatsby-standard'],
   plugins: ['json'],
+  // rules: {
+  //   // Uncomment this if related styled components or other css-in-js stuff causes linting errors.
+  //   'no-unused-expressions': ['error', {allowTaggedTemplates: true}],
+  // },
 }
