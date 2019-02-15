3dd14e0298cf04333911ba247a097aa2f43dd755
Jason Cady <jamamuuga@gmail.com>
2019-02-14 23:06:35 -0800

    Change comment references from Rebass to Material UI in Navbar Style Component & theme Colors style component.

--- src/styles/components/navbar-style.js
+++ src/styles/components/navbar-style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-// TODO: Make sure this style will work well with Styled System themes on a Rebass Navbar.
+// TODO: Make sure this style will work well with Styled System themes on a Material UI Navbar.
 // FIXME: What is the right constructor?
 const NavbarStyle = styled.div`
   .navbar .navbar-menu {
--- src/styles/themes/colors.js
+++ src/styles/themes/colors.js
@@ -1,6 +1,6 @@
 // Config vars
 
-// TODO: Port this to Rebass maybe using styled-system and/or styled-components-theme
+// TODO: Port this to Material UI maybe using styled-system and/or styled-components-theme
 
 // Kaldi logo brand color: #FD461E
 
