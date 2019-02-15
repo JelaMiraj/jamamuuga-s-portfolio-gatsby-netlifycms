fac93b538de8c4144df0730d1a5a4165241d1459
Jason Cady <jamamuuga@gmail.com>
2019-02-14 23:04:10 -0800

    Comment out current custom Grid Container & Grid components because they use @rebass/grid.

--- src/components/Grid/Container.js
+++ src/components/Grid/Container.js
@@ -1,22 +1,23 @@
-import PropTypes from 'prop-types'
-import styled from 'util/style'
-import {Box} from '@rebass/grid'
+// import PropTypes from 'prop-types'
+// import styled from 'styled-components'
+// import {Box} from '@rebass/grid'
 
-const Container = styled(Box)`
-  max-width: ${props => props.maxWidth};
-`
+// // TODO: Shouldn't we just use Material UI's Container?
+// const Container = styled(Box)`
+//   max-width: ${props => props.maxWidth};
+// `
 
-Container.propTypes = {
-  maxWidth: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string,
-    PropTypes.array,
-  ]),
-}
+// Container.propTypes = {
+//   maxWidth: PropTypes.oneOfType([
+//     PropTypes.number,
+//     PropTypes.string,
+//     PropTypes.array,
+//   ]),
+// }
 
-Container.defaultProps = {
-  mx: 'auto',
-  maxWidth: '700px',
-}
+// Container.defaultProps = {
+//   mx: 'auto',
+//   maxWidth: '700px',
+// }
 
-export default Container
+// export default Container
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,4 +1,4 @@
-import {Flex, Box} from '@rebass/grid'
-import Container from './Container'
+// import {Flex, Box} from '@rebass/grid'
+// import Container from './Container'
 
-export {Flex, Box, Container}
+// export {Flex, Box, Container}
