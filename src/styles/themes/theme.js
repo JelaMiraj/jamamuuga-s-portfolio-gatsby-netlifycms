2d15049fc063c078f4277efd92271cb500a3fc16
Jason Cady <jamamuuga@gmail.com>
2019-02-21 21:06:16 -0800

    Updated external theme to be compatible with current version of Material UI.

--- src/styles/themes/theme.js
+++ src/styles/themes/theme.js
@@ -1,38 +1,40 @@
 // Setup base Material-UI theme
-import createMuiTheme from 'material-ui/styles/createMuiTheme'
-import purple from 'material-ui/colors/purple'
-import green from 'material-ui/colors/green'
-import red from 'material-ui/colors/red'
-import grey from 'material-ui/colors/grey'
+import {createMuiTheme} from '@material-ui/core/styles'
+import purple from '@material-ui/core/colors/purple'
+import green from '@material-ui/core/colors/green'
+import red from '@material-ui/core/colors/red'
+import grey from '@material-ui/core/colors/grey'
 // TODO: Update this Material UI Them from v1 to v3 & v4 compatible.
 
 const headlineFont = `Merriweather, Georgia, serif`
 const bodyFont = `"Open Sans", Roboto, "Helvetica Neue", Arial, sans-serif`
 
-export default createMuiTheme({
+const theme = createMuiTheme({
   palette: {
     primary: purple,
     secondary: green,
     error: red,
   },
   typography: {
+    // TODO: Stop repeating greys, font weights, & font families.
+    useNextVariants: true,
     fontFamily: bodyFont,
     fontSize: 14, // default
     // color: grey[500],
     fontWeightLight: 300,
     fontWeightRegular: 400,
     fontWeightMedium: 600,
-    display1: {
+    h4: {
       fontFamily: headlineFont,
       color: grey[900],
     },
-    display2: {fontFamily: headlineFont, color: grey[900]},
-    display3: {fontFamily: headlineFont, color: grey[900]},
-    display4: {fontFamily: headlineFont, color: grey[900]},
-    headline: {fontFamily: headlineFont, color: grey[900]},
-    subheading: {fontFamily: headlineFont, color: grey[900]},
-    title: {fontFamily: headlineFont, color: grey[900], fontWeight: 400},
-    body1: {
+    h3: {fontFamily: headlineFont, color: grey[900]},
+    h2: {fontFamily: headlineFont, color: grey[900]},
+    h1: {fontFamily: headlineFont, color: grey[900]},
+    h5: {fontFamily: headlineFont, color: grey[900]},
+    subtitle1: {fontFamily: headlineFont, color: grey[900]},
+    h6: {fontFamily: headlineFont, color: grey[900], fontWeight: 400},
+    body2: {
       fontFamily: bodyFont,
       color: grey[700],
       fontWeight: 400,
@@ -52,3 +54,5 @@ export default createMuiTheme({
     },
   },
 })
+
+export default theme
