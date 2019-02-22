83c054c9cac1448dbe0f1cba63907c57123337f0
Jason Cady <jamamuuga@gmail.com>
2019-02-21 21:14:44 -0800

    Autoformatted custom Link component.

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import MuiLink from '@material-ui/core/Link';
-import { Link as GastsbyLink } from 'gatsby';
+import React from 'react'
+import MuiLink from '@material-ui/core/Link'
+import {Link as GastsbyLink} from 'gatsby'
 
 function Link(props) {
-  return <MuiLink component={GastsbyLink} {...props} />;
+  return <MuiLink component={GastsbyLink} {...props} />
 }
 
-export default Link;
+export default Link
