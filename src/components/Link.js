8456687cc76f273ee900870814b7508afb155094
Jason Cady <jamamuuga@gmail.com>
2019-02-21 21:33:17 -0800

    Added comment about original source of custom link component.

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,3 +1,4 @@
+// See https://github.com/mui-org/material-ui/tree/next/examples/gatsby-next
 import React from 'react'
 import MuiLink from '@material-ui/core/Link'
 import {Link as GatsbyLink} from 'gatsby'
