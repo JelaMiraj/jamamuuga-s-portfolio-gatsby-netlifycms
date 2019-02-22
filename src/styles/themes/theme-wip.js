7469239df2910c826e0a3bb127a3f6f4daa25904
Jason Cady <jamamuuga@gmail.com>
2019-02-21 18:38:42 -0800

    Switch to out-dated Material UI based theme from Procyon.

---
+++ src/styles/themes/theme-wip.js
@@ -0,0 +1,6 @@
+import {colors as themeColors} from './colors'
+// TODO: Make sure the final object has the correct structure and not something wierd like embedding an object in an object instead of assigning properties.
+
+export default {
+  colors: themeColors,
+}
--- src/styles/themes/theme.js
+++ src/styles/themes/theme.js
@@ -1,6 +1,53 @@
-import {colors as themeColors} from './colors'
-// TODO: Make sure the final object has the correct structure and not something wierd like embedding an object in an object instead of assigning properties.
+// Setup base Material-UI theme
+import createMuiTheme from 'material-ui/styles/createMuiTheme';
+import purple from 'material-ui/colors/purple';
+import green from 'material-ui/colors/green';
+import red from 'material-ui/colors/red';
+import grey from 'material-ui/colors/grey';
 
-export default {
-  colors: themeColors,
-}
+const headlineFont = `Merriweather, Georgia, serif`;
+const bodyFont = `"Open Sans", Roboto, "Helvetica Neue", Arial, sans-serif`;
+
+export default createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+    error: red,
+  },
+  typography: {
+    fontFamily: bodyFont,
+    fontSize: 14, // default
+    // color: grey[500],
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 600,
+    display1: {
+      fontFamily: headlineFont,
+      color: grey[900],
+    },
+    display2: { fontFamily: headlineFont, color: grey[900] },
+    display3: { fontFamily: headlineFont, color: grey[900] },
+    display4: { fontFamily: headlineFont, color: grey[900] },
+    headline: { fontFamily: headlineFont, color: grey[900] },
+    subheading: { fontFamily: headlineFont, color: grey[900] },
+    title: { fontFamily: headlineFont, color: grey[900], fontWeight: 400 },
+    body1: {
+      fontFamily: bodyFont,
+      color: grey[700],
+      fontWeight: 400,
+      margin: '1em 0',
+      '& h2': { fontFamily: headlineFont, color: grey[900], fontWeight: 400 },
+      '& h3': { fontFamily: headlineFont, color: grey[900], fontWeight: 400 },
+    },
+  },
+  overrides: {
+    MuiGrid: {
+      'spacing-xs-16': {
+        margin: '0 -8px',
+        '& > *[class^=MuiGrid-typeItem]': {
+          padding: '0 8px',
+        },
+      },
+    },
+  },
+});
