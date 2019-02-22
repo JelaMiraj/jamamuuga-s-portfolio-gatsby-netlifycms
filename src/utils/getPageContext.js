d4ce34401afe377993076ef2ad2f6455624b417e
Jason Cady <jamamuuga@gmail.com>
2019-02-21 21:05:05 -0800

    Refer to external theme in getPageContext utility.

--- src/utils/getPageContext.js
+++ src/utils/getPageContext.js
@@ -1,28 +1,31 @@
 // See https://github.com/mui-org/material-ui/tree/master/examples/gatsby
 import {SheetsRegistry} from 'jss'
-import {createMuiTheme, createGenerateClassName} from '@material-ui/core/styles'
-import purple from '@material-ui/core/colors/purple'
-import green from '@material-ui/core/colors/green'
+// import {createMuiTheme, createGenerateClassName} from '@material-ui/core/styles'
+import {createGenerateClassName} from '@material-ui/core/styles'
+// import purple from '@material-ui/core/colors/purple'
+// import green from '@material-ui/core/colors/green'
+import theme from '../styles/themes/theme'
 
-// A theme with custom primary and secondary color.
-// It's optional.
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
-    },
-    secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700],
-    },
-  },
-  typography: {
-    useNextVariants: true,
-  },
-})
+// // FIXME: refer to external theme instead of making it here.
+// // A theme with custom primary and secondary color.
+// // It's optional.
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       light: purple[300],
+//       main: purple[500],
+//       dark: purple[700],
+//     },
+//     secondary: {
+//       light: green[300],
+//       main: green[500],
+//       dark: green[700],
+//     },
+//   },
+//   typography: {
+//     useNextVariants: true,
+//   },
+// })
 
 function createPageContext() {
   return {
