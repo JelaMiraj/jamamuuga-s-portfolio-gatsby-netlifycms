01ad37efdb75582b9bed515ec3114e7b4928f56d
Jason Cady <jamamuuga@gmail.com>
2019-02-28 16:35:21 -0800

    Combine, comment out, & better document 2 copied Gatsby Browser configs.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,20 @@
-// // It's not ready yet: https://github.com/gatsbyjs/gatsby/issues/8237.
-
 // import React from 'react'
 // import withRoot from './src/utils/withRoot'
+// import ReactDOM from 'react-dom'
+
+// Begin Mui Gatsby Example Body
+// From: https://github.com/mui-org/material-ui/blob/master/examples/gatsby/gatsby-browser.js
+
+// It's not ready yet: https://github.com/gatsbyjs/gatsby/issues/8237.
 
 // const WithRoot = withRoot(props => props.children)
 
 // export const wrapRootElement = ({element}) => <WithRoot key={Math.random()}>{element}</WithRoot>
+// End Mui Gatsby Example Body
+
+// Begin Gatsby MUI Business Starter Body
+// NOTE: Hopefully this isn't needed anymore. Re-enable if styles get messed up on page refresh.
+// From: https://github.com/bluepeter/gatsby-material-ui-business-starter/blob/master/gatsby-browser.js
+// export const replaceHydrateFunction = () => (element, container, callback) =>
+//   ReactDOM.render(element, container, callback)
+// End Gatsby MUI Business Starter Body
