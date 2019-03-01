1c4c09352935c5809985181ca3e8e38dc1e31607
Jason Cady <jamamuuga@gmail.com>
2019-02-28 19:06:57 -0800

    Added comment about link component confusion in link component.

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import MuiLink from '@material-ui/core/Link'
 import {Link as GatsbyLink} from 'gatsby'
+// TODO: What is the difference between Link from gatsby and gatsby-link.
 
 function Link(props) {
   return <MuiLink component={GatsbyLink} {...props} />
