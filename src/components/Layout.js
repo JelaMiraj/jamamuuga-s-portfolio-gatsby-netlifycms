d42e669ae8f414e8ee00a7e6b074d15d0c74c3e8
Jason Cady <jamamuuga@gmail.com>
2019-03-07 21:45:43 -0800

    Added Material UI theme system to Layout component.
    NOTE: It is probably best to only add theme from the page level & not this layout level.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,22 @@
 import React, {Fragment} from 'react'
 // FIXME: Switch to MUI Themes.
-import {ThemeProvider} from 'styled-components'
+// import {ThemeProvider} from 'styled-components'
+import {MUIThemeProvider, withStyles} from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 import {ScrollingProvider} from 'react-scroll-section'
 // import GlobalStyle from '../styles/global-style'
 // TODO: Import a proper Styled System theme here instead of a separate pure color object.
-import colors from '../styles/themes/colors'
+// import colors from '../styles/themes/colors'
+import Header from './Header'
 import Helmet from './Helmet'
 // TODO: Why is this navigating down & back up to same folder?
 import Navbar from './Navbar'
+import mytheme from '../styles/themes/theme'
 
+// TODO: Find out if we need any <GlobalStyle />
 const Layout = ({children}) => (
   <Fragment>
-    {/* <GlobalStyle /> */}
-    <ThemeProvider theme={{colors}}>
+    <MUIThemeProvider theme={mytheme}>
       <ScrollingProvider>
         <div>
           <Helmet />
@@ -21,7 +24,7 @@ const Layout = ({children}) => (
           <div>{children}</div>
         </div>
       </ScrollingProvider>
-    </ThemeProvider>
+    </MUIThemeProvider>
   </Fragment>
 )
 
