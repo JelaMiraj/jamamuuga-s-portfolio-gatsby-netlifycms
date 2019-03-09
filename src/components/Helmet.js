4e02b5301f0db5cb84c3047c915f33ae55b88ca7
Jason Cady <jamamuuga@gmail.com>
2019-03-08 19:01:18 -0800

    Ported more of the SEO component over to the Helmet component.
    Git's diff for this commit is especially sloppy...

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -11,60 +11,62 @@ import PropTypes from 'prop-types'
 // const Helmet = ({theme = {}}) => (
 const Helmet = props => (
   <StaticQuery
-    query={graphql`
-      query HeadingQuery {
-        site {
-          siteMetadata {
-            title
-            description
-          }
-        }
-      }
-    `}
-    render={data => (
-      <ReactHelmet>
-        <html lang="en" />
-        <title>{data.site.siteMetadata.title}</title>
-        <meta name="description" content={data.site.siteMetadata.description} />
+    query={detailsQuery}
+    render={data => {
+      const title = props.title || data.site.siteMetadata.title
+      const description =
+        props.description || data.site.siteMetadata.description
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/img/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/img/favicon-16x16.png"
-          sizes="16x16"
-        />
+      return (
+        <ReactHelmet
+          htmlAttributes={{
+            lang: 'en',
+          }}
+          title={title}
+          titleTemplate={`%s - ${title}`}
+        >
+          <meta name="description" content={description} />
 
-        <link
-          rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
-          color="#ff4400"
-        />
-        <meta name="theme-color" content="#fff" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/img/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/img/favicon-16x16.png"
+            sizes="16x16"
+          />
 
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={data.site.siteMetadata.title} />
-        <meta property="og:url" content="/" />
-        <meta property="og:image" content="/img/og-image.jpg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1,
+          <link
+            rel="mask-icon"
+            href="/img/safari-pinned-tab.svg"
+            color="#ff4400"
+          />
+          <meta name="theme-color" content="#fff" />
+
+          <meta property="og:type" content="business.business" />
+          <meta property="og:title" content={title} />
+          <meta property="og:url" content="/" />
+          <meta property="og:image" content="/img/og-image.jpg" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1,
             initial-scale=1,
             width=device-width,
             shrink-to-fit=no"
-        />
-      </ReactHelmet>
-    )}
+          />
+          {props.children}
+        </ReactHelmet>
+      )
+    }}
   />
 )
 
@@ -72,6 +74,23 @@ Helmet.propTypes = {
   // eslint-disable-next-line
   theme: PropTypes.object,
 }
+
+Helmet.defaultProps = {
+  lang: 'en',
+  meta: [],
+  keywords: [],
+}
+
+const detailsQuery = graphql`
+  query DefaultHelmetQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
 
 // export default withStyles(Helmet)
 export default Helmet
