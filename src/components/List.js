9ddc90351cefd3ce11ab74bb33d3141a8d4d2a40
Jason Cady <jamamuuga@gmail.com>
2019-03-14 16:04:16 -0700

    Switch to custom Link component in List component.

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import {Link, withPrefix} from 'gatsby'
+// import {Link, withPrefix} from 'gatsby'
+import {withPrefix} from 'gatsby'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
+import Link from './Link'
 
 const List = props => (
   <Grid
