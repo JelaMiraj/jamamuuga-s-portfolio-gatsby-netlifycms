32676856354b5b816252c3c82876561efa82fd47
Jason Cady <jamamuuga@gmail.com>
2019-03-15 09:58:50 -0700

    Added Pagination & PostItem components from Gatsby Netlify Identity Starter.

---
+++ src/components/Pagination.js
@@ -0,0 +1,58 @@
+import React from 'react'
+// import {Link} from 'gatsby'
+import Link from './Link'
+// Pagination for posts.
+
+const ulstyle = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  listStyle: 'none',
+}
+
+const lkstyle = (i, currentPage) => ({
+  padding: 10,
+  textDecoration: 'none',
+  color: i + 1 === currentPage ? '#ffffff' : '#D64000',
+  background: i + 1 === currentPage ? '#D64000' : '',
+})
+
+const Pagination = ({currentPage, numPages}) => {
+  const isFirst = currentPage === 1
+  const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+  const isLast = currentPage === numPages
+  const nextPage = (currentPage + 1).toString()
+
+  return (
+    <ul style={ulstyle}>
+      {!isFirst && (
+        <Link to={prevPage} rel="prev" className="has-text-primary">
+          ← Previous Page
+        </Link>
+      )}
+      {Array.from({length: numPages}, (_, i) => (
+        <li
+          key={`pagination-number${i + 1}`}
+          style={{
+            margin: 0,
+          }}
+        >
+          <Link to={`/${i === 0 ? '' : i + 1}`} style={lkstyle(i, currentPage)}>
+            {i + 1}
+          </Link>
+        </li>
+      ))}
+      {!isLast && (
+        <Link to={nextPage} rel="next" className="has-text-primary">
+          Next Page →
+        </Link>
+      )}
+    </ul>
+  )
+}
+
+// TODO, add propTypes
+// Pagination.propTypes = {}
+
+export default Pagination
---
+++ src/components/PostItem.js
@@ -0,0 +1,49 @@
+import React from 'react'
+// import {Link} from 'gatsby'
+import Link from './Link'
+// Intended for use in site or blog index page.
+
+const PostItem = ({post, logged}) => {
+  const cntBorder = {
+    // border: '1px solid #888',
+    borderBottomWidth: 1,
+    borderBottomColor: '#888',
+    borderBottomStyle: 'solid', // this attribute doesn't auto complete???
+    padding: '1em 0',
+    display: 'flex', // horizontal align thumbnail and text ...
+  }
+
+  return (
+    <div className="content" style={cntBorder} key={post.id}>
+      {/** @2018/12/17 */}
+      <img
+        src={post.frontmatter.image.childImageSharp.fluid.src}
+        className="thumbnail-img"
+        alt="post thumbnail"
+      />
+      <div className="post-item">
+        <p className="post-title">
+          <Link
+            className={logged ? 'has-text-primary' : 'has-text-unlogin'}
+            to={logged ? post.fields.slug : '/login'}
+          >
+            {post.frontmatter.title}
+          </Link>
+          <span> &bull; </span>
+          <small>{post.frontmatter.date}</small>
+        </p>
+        <p>
+          <span className="post-excerpt">{post.excerpt}</span>
+          <Link
+            className={logged ? 'button is-small orange' : 'button is-small'}
+            to={logged ? post.fields.slug : '/login'}
+          >
+            Keep Reading →
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default PostItem
