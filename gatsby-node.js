2241ba89d3089bcb0cb7db825e3c90d063612fa2
Jason Cady <jamamuuga@gmail.com>
2019-03-15 09:23:27 -0700

    Added title to passed frontmatter data.

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,6 +17,7 @@ exports.createPages = ({ actions, graphql }) => {
             }
             frontmatter {
               tags
+              title
               templateKey
             }
           }
