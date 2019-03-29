edbea95ab243ef17233333d582c904ad404de09d
Jason Cady <jamamuuga@gmail.com>
2019-03-29 01:48:28 -0700

    Autoformatted custom List component.
    Hopefully it isn't broken from autoformatting.

--- src/components/List.js
+++ src/components/List.js
@@ -8,45 +8,44 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import Link from './Link'
 
-const List = props => {
-  return (
-    <Grid
-      spacing={24}
-      container
-      direction="row"
-      alignItems="flex-start"
-      justify="center"
-    >
-      {props.items.map(edge => {
-        const {
-          node: {
-            excerpt,
-            frontmatter: {
-              path,
-              title,
-              image: { publicURL },
-            },
+const List = props => (
+  <Grid
+    spacing={24}
+    container
+    direction="row"
+    alignItems="flex-start"
+    justify="center"
+  >
+    {props.items.map(edge => {
+      const {
+        node: {
+          excerpt,
+          frontmatter: {
+            path,
+            title,
+            image: { publicURL },
           },
-        } = edge
-        return (
-          <Grid item xs={12} md={6} key={path}>
-            <Card>
-              <CardMedia
-                style={{ height: '200px' }}
-                image={withPrefix(publicURL)}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  <Link to={path}>{title}</Link>
-                </Typography>
-                <Typography component="p">{excerpt}</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        )
-      })}
-    </Grid>
-  )
-}
+        },
+      } = edge
+
+      return (
+        <Grid item xs={12} md={6} key={path}>
+          <Card>
+            <CardMedia
+              style={{ height: '200px' }}
+              image={withPrefix(publicURL)}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                <Link to={path}>{title}</Link>
+              </Typography>
+              <Typography component="p">{excerpt}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      )
+    })}
+  </Grid>
+)
 
 export default List
