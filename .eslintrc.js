aed33ec7491ab50e5a27ea6c700d9a47dde3d2dd
Jason Cady <jamamuuga@gmail.com>
2019-03-28 18:27:48 -0700

    Force ESLint to match Prettier's object curly spacing style.
    Unfortunately, eslint-config-prettier missed this.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,10 @@ module.exports = {
   },
   extends: ['eslint-config-with-prettier', 'eslint-config-gatsby-standard'],
   plugins: ['json'],
-  // rules: {
-  //   // Uncomment this if related styled components or other css-in-js stuff causes linting errors.
-  //   'no-unused-expressions': ['error', {allowTaggedTemplates: true}],
-  // },
+  rules: {
+    // // Uncomment this if related styled components or other css-in-js stuff causes linting errors.
+    // 'no-unused-expressions': ['error', { allowTaggedTemplates: true }],
+    // Force object curly spacing to match Prettier config. Somehow eslint-config-with-prettier misses this.
+    'object-curly-spacing': ['error', 'always'],
+  },
 }
