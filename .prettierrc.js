4f77d679a10943a6c356dad5c5ea381e0873b2d6
Jason Cady <jamamuuga@gmail.com>
2019-03-28 18:01:19 -0700

    Remove es5 trailing comma override in Prettier config because Node excepts trailing commas in function parameters.

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,13 +4,4 @@ module.exports = {
   // trailingComma: 'es5',
   trailingComma: 'all',
   // bracketSpacing: false,
-  overrides: [
-    {
-      // Add any es5 config files here to avoid breaking on es6 more trailing commas.
-      files: 'gatsby-*.js',
-      options: {
-        trailingComma: 'es5',
-      },
-    },
-  ],
 }
