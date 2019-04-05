260de56404c10a780ef3d9ca1dcae68c81cbf9c9
Jason Cady <jamamuuga@gmail.com>
2019-04-04 21:10:03 -0700

    Add Box & condense Material UI imports in Features component.

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,8 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Grid from "@material-ui/core/Grid"
-import Paper from "@material-ui/core/Paper"
-import Typography from "@material-ui/core/Typography"
+import {Box, Grid, Paper, Typography} from "@material-ui/core"
 import PreviewCompatibleImage from "./PreviewCompatibleImage"
 
 const FeatureGrid = ({gridItems}) => (
