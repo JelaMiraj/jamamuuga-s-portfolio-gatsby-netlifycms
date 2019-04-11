c177b2c06cb19ffcb77f50e5abe108ebfbd33a92
Jason Cady <jamamuuga@gmail.com>
2019-04-11 03:44:12 -0700

    Update Global Style to use CssBaseline fROM Material UI.

--- src/styles/global-style.js
+++ src/styles/global-style.js
@@ -1,14 +1,21 @@
-import styled, {createGlobalStyle} from "styled-components"
-import reboot from "styled-reboot"
+import React from "react"
+import {CssBaseline} from "@material-ui/core"
+// import styled, {createGlobalStyle} from "styled-components"
+// import reboot from "styled-reboot"
 // TODO: Import a proper Styled System theme here instead of a separate pure color object.
 import colors from "./themes/colors"
 
 const options = {}
 
-const rebootCss = reboot(/* options */)
+// const rebootCss = reboot(/* options */)
 
-const GlobalStyle = createGlobalStyle`
-  ${rebootCss}
-`
+// const GlobalStyle = createGlobalStyle`
+//   ${rebootCss}
+// `
+const GlobalStyle = () => (
+  <>
+    <CssBaseline />
+  </>
+)
 
 export default GlobalStyle
