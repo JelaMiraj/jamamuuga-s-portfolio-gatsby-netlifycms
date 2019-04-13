a46ff7c0f61a2d54a6b0f9d061de75659995f768
Jason Cady <jamamuuga@gmail.com>
2019-04-13 13:27:47 -0700

    Consolidated imports & exports, moved withRoot to top, cleaned up, Convert more to Material UI, added named export,

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,19 +1,26 @@
+// eslint-disable-next-line
+import withRoot from "../tools/withRoot"
+// eslint-disable-next-line
 import React from "react"
 import PropTypes from "prop-types"
-// import {Link, graphql} from "gatsby"
 import {graphql} from "gatsby"
-import {Card, Button, Grid, Paper, Typography} from "@material-ui/core"
+import {
+  Container,
+  CardAction,
+  CardContent,
+  Button,
+  Grid,
+  Paper,
+  Typography,
+} from "@material-ui/core"
 import {unstable_Box as Box} from "@material-ui/core/Box"
-// import {withStyles} from "@material-ui/core/styles"
-import {makeStyles, withStyles} from "@material-ui/core/styles"
+import Card from "../components/Card"
 import Layout from "../components/Layout"
 import Link from "../components/Link"
 import Features from "../components/Features"
 import BlogRoll from "../components/BlogRoll"
-import mytheme from "../styles/themes/theme"
-import withRoot from "../utils/withRoot"
 
-// TODO: Convert everything to use React Hooks (Don't forget the ESLint plugin), Babel-Blade (DRY GraphQL), & functions not classes.
+// TODO: Convert everything to use Babel-Blade (DRY GraphQL).
 
 // FIXME: complains about object is not a function....
 // const useStyles = makeStyles(theme => ({
@@ -22,7 +29,7 @@ import withRoot from "../utils/withRoot"
 //     paddingTop: theme.spacing(20),
 //   },
 // }))
-export const IndexPageTemplate = ({
+const IndexPageTemplate = ({
   image,
   title,
   heading,
@@ -30,15 +37,15 @@ export const IndexPageTemplate = ({
   mainpitch,
   description,
   intro,
-  main,
 }) => {
   // TODO: Delete this null variable when eslint stops reformatting and breaking this function.
+  // eslint-disable-next-line
   const noOp = null
   /* const classes = useStyles() */
 
   return (
-    <div>
-      <div
+    <Box>
+      <Box
         className="full-width-image margin-top-0"
         style={{
           backgroundImage: `url(${
@@ -48,7 +55,7 @@ export const IndexPageTemplate = ({
           backgroundAttachment: `fixed`,
         }}
       >
-        <div
+        <Box
           style={{
             display: "flex",
             height: "150px",
@@ -60,7 +67,8 @@ export const IndexPageTemplate = ({
         >
           <Typography
             variant="h1"
-            className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+            fontWeight="fontWeightBold"
+            className="is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
             style={{
               boxShadow:
                 "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
@@ -74,7 +82,8 @@ export const IndexPageTemplate = ({
           </Typography>
           <Typography
             variant="h3"
-            className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+            fontWeight="fontWeightBold"
+            className="is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
             style={{
               boxShadow:
                 "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
@@ -86,32 +95,34 @@ export const IndexPageTemplate = ({
           >
             {subheading}
           </Typography>
-        </div>
-      </div>
-      <section className="section section--gradient">
-        <Box className="container">
-          <section className="section">
-            <Grid container className="columns">
-              <Grid item xs={10} className="column is-10 is-offset-1">
-                <Box className="content">
-                  <Box className="content">
-                    <Paper className="tile">
-                      <Typography variant="h1" className="title">
-                        {mainpitch.title}
-                      </Typography>
-                    </Paper>
+        </Box>
+      </Box>
+      <Box component="section" className="section--gradient">
+        <Container>
+          <Box component="section">
+            <Grid container>
+              <Grid item xs={10} className="is-offset-1">
+                <Box>
+                  <Box>
+                    <Card className="tile">
+                      <CardContent>
+                        <Typography variant="h1" className="title">
+                          {mainpitch.title}
+                        </Typography>
+                      </CardContent>
+                    </Card>
                     <Paper className="tile">
                       <Typography variant="h3" className="subtitle">
                         {mainpitch.description}
                       </Typography>
                     </Paper>
                   </Box>
-                  <Grid container className="columns">
-                    <Grid item xs={12} className="column">
+                  <Grid container>
+                    <Grid item xs={12}>
                       <Typography
                         variant="h3"
                         fontWeight="fontWeightSemiBold"
-                        className="is-size-2"
+                        fontSize="fontSize2"
                       >
                         {heading}
                       </Typography>
@@ -119,8 +130,8 @@ export const IndexPageTemplate = ({
                     </Grid>
                   </Grid>
                   <Features gridItems={intro.blurbs} />
-                  <Grid container className="columns">
-                    <Grid item xs={12} align="center" className="column">
+                  <Grid container>
+                    <Grid item xs={12} align="center">
                       <Button variant="contained" color="secondary">
                         <Link className="btn" to="/products">
                           See all products
@@ -128,16 +139,16 @@ export const IndexPageTemplate = ({
                       </Button>
                     </Grid>
                   </Grid>
-                  <Grid item xs={12} className="column">
+                  <Grid item xs={12}>
                     <Typography
                       variant="h3"
                       fontWeight="fontWeightSemiBold"
-                      className="is-size-2"
+                      fontSize="fontSize2"
                     >
                       Latest stories
                     </Typography>
                     <BlogRoll />
-                    <Grid item xs={12} align="center" className="column">
+                    <Grid item xs={12} align="center">
                       <Button variant="contained" color="secondary">
                         <Link className="btn" to="/blog">
                           Read more
@@ -148,10 +159,10 @@ export const IndexPageTemplate = ({
                 </Box>
               </Grid>
             </Grid>
-          </section>
-        </Box>
-      </section>
-    </div>
+          </Box>
+        </Container>
+      </Box>
+    </Box>
   )
 }
 
@@ -193,11 +204,9 @@ IndexPage.propTypes = {
   }),
 }
 
-// FIXME: Somehow, trying to use withStyles this way fails
-// export default withRoot(withStyles(mytheme)(IndexPage))
-export default withRoot(IndexPage)
+const IndexPageWrapped = withRoot(IndexPage)
 
-export const pageQuery = graphql`
+const PageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: {templateKey: {eq: "index-page"}}) {
       frontmatter {
@@ -234,3 +243,6 @@ export const pageQuery = graphql`
     }
   }
 `
+
+export default IndexPageWrapped
+export {IndexPageWrapped as IndexPage, IndexPageTemplate, PageQuery}
