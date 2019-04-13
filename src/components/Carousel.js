dc37598a217c7646eac7c31318043a515deb849a
Jason Cady <jamamuuga@gmail.com>
2019-04-13 08:43:42 -0700

    Consolidated imports & added named export to Carousel.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,14 +1,16 @@
 import React from "react"
 import {withPrefix, Link} from "gatsby"
-import MobileStepper from "@material-ui/core/MobileStepper"
-import Paper from "@material-ui/core/Paper"
-import Typography from "@material-ui/core/Typography"
-import Button from "@material-ui/core/Button"
+import {
+  CardContent,
+  CardMedia,
+  Button,
+  MobileStepper,
+  Paper,
+  Typography,
+} from "@material-ui/core"
 import SwipeableViews from "react-swipeable-views"
-import Card from "@material-ui/core/Card"
-import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
 import {autoPlay} from "react-swipeable-views-utils"
+import Card from "./Card"
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
@@ -112,3 +114,4 @@ class Carousel extends React.Component {
 }
 
 export default Carousel
+export {Carousel}
