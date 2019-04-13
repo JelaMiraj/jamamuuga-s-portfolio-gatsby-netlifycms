7731568d0befc2ee77973aef63db10233e75a8d8
Jason Cady <jamamuuga@gmail.com>
2019-04-13 10:54:53 -0700

    Consolidate imports, add named exports, clean up, & update based on upstream in Page component.

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,45 +1,46 @@
-import React from "react"
-// import {Grid, Row, Col} from "react-flexbox-grid"
+import React, {Component} from "react"
 import PropTypes from "prop-types"
-// import {MUIThemeProvider, withStyles} from "@material-ui/core/styles"
-import Paper from "@material-ui/core/Paper"
-import Grid from "@material-ui/core/Grid"
-import Typography from "@material-ui/core/Typography"
+import {Grid, Typography} from "@material-ui/core"
 import Header from "./Header"
 import Footer from "./Footer"
-// import theme from "../styles/themes/theme"
-// TODO: Using MUIThemeProvider & styling here might not be the right thing with Material UI. Probably best to add theme only from the page level & not this layout level.
-// FIXME: using MUIThemeProvider here breaks things...
 
-class Page extends React.Component {
+class Page extends Component {
   render() {
     const {title, children} = this.props
 
     return (
       <>
-        {/* <MUIThemeProvider theme={theme}> */}
         <Header />
-        <Grid container style={{marginTop: "94px"}}>
-          <Grid item container direction="row">
-            <Grid item container direction="column" xs={12}>
-              {title ? (
-                <Typography
-                  variant="h2"
-                  gutterBottom
-                  style={{textAlign: "center"}}
-                >
-                  {title}
-                </Typography>
-              ) : null}
-              {children}
-              <Footer />
-            </Grid>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          style={{marginTop: "94px"}}
+        >
+          <Grid
+            item
+            style={{
+              maxWidth: "calc(1136px - 60px)",
+              width: "calc(100% - 60px)",
+            }}
+          >
+            {title ? (
+              <Typography
+                variant="h2"
+                gutterBottom
+                style={{textAlign: "center"}}
+              >
+                {title}
+              </Typography>
+            ) : null}
+            {children}
+            <Footer />
           </Grid>
         </Grid>
-        {/* </MUIThemeProvider> */}
       </>
     )
   }
 }
 
 export default Page
+export {Page}
