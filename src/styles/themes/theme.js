5173df19cfbf6722cbe28f05ba60045070eb375f
Jason Cady <jamamuuga@gmail.com>
2019-04-13 02:37:28 -0700

    Import temporary install helper from Material UI Stylles in theme.

--- src/styles/themes/theme.js
+++ src/styles/themes/theme.js
@@ -1,5 +1,5 @@
 // Setup base Material-UI theme
-import {createMuiTheme} from "@material-ui/core/styles"
+import {createMuiTheme, install} from "@material-ui/styles"
 import purple from "@material-ui/core/colors/purple"
 import green from "@material-ui/core/colors/green"
 import red from "@material-ui/core/colors/red"
