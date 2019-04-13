357e696421af2ed8cd086455ac1c1b0a436926cb
Jason Cady <jamamuuga@gmail.com>
2019-04-13 11:40:12 -0700

    Moved withRoot to top & fix folder name, used more Material UI components, & added named exports in 404 NotFoundPage.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,24 @@
+// eslint-disable-next-line
+import withRoot from "../tools/withRoot"
+// eslint-disable-next-line
 import React from "react"
+import {Typography} from "@material-ui/core"
+import {unstable_Box as Box} from "@material-ui/core/Box"
 import Layout from "../components/Layout"
-import withRoot from "../utils/withRoot"
 
 const NotFoundPage = () => (
   <Layout title="Not Found">
-    <div>
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist...</p>
-    </div>
+    <Box>
+      <Typography variant="h1">Not Found</Typography>
+      <Typography paragraph>
+        You just hit a route that doesn&#39;t exist...
+      </Typography>
+    </Box>
   </Layout>
 )
 
 // TODO: Make sure withRoot call isn't breaking stuff.
-export default withRoot(NotFoundPage)
+const NotFoundPageWrapped = withRoot(NotFoundPage)
+
+export default NotFoundPageWrapped
+export {NotFoundPageWrapped as NotFoundPage}
