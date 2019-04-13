8af01c3e6bbd9d9ad7e0546f2413f351642ef979
Jason Cady <jamamuuga@gmail.com>
2019-04-13 08:17:00 -0700

    Temporarily comment out some Babel config stuff until compile issues are sorted out better.

--- .babelrc.js
+++ .babelrc.js
@@ -3,17 +3,17 @@ module.exports = {
     "@babel/preset-env",
     [
       "@babel/preset-react",
-      {
-        development: process.env.BABEL_ENV === "development",
-      },
+      // {
+      //   development: process.env.BABEL_ENV === "development",
+      // },
     ],
     "babel-preset-gatsby",
   ],
   plugins: [
-    "@babel/plugin-proposal-decorators",
-    "@babel/plugin-proposal-class-properties",
-    "babel-plugin-root-import",
-    "babel-plugin-transform-imports",
+    // "@babel/plugin-proposal-decorators",
+    // "@babel/plugin-proposal-class-properties",
+    // "babel-plugin-root-import",
+    // "babel-plugin-transform-imports",
     "@babel/plugin-syntax-dynamic-import",
     [
       "named-params",
