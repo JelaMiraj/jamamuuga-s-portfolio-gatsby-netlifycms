9ab7ed9c26f353541114f7c3cfd8f1b324b755c2
Jason Cady <jamamuuga@gmail.com>
2019-04-13 11:21:22 -0700

    Converted to more appropriate components & added named export in PostItem.

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,8 +1,10 @@
 import React from "react"
-// import {Link} from "gatsby"
-import Typography from "@material-ui/core"
+import {Typography} from "@material-ui/core"
+import {unstable_Box as Box} from "@material-ui/core/Box"
+import {PreviewCompatibleImage as Image} from "./PreviewCompatibleImage"
 import Link from "./Link"
 // Intended for use in site or blog index page.
+// TODO: Maybe use Card if not wrapped by caller.
 
 const PostItem = ({post, logged}) => {
   const cntBorder = {
@@ -15,14 +17,14 @@ const PostItem = ({post, logged}) => {
   }
 
   return (
-    <div className="content" style={cntBorder} key={post.id}>
+    <Box className="content" style={cntBorder} key={post.id}>
       {/** @2018/12/17 */}
-      <img
+      <Image
         src={post.frontmatter.image.childImageSharp.fluid.src}
         className="thumbnail-img"
         alt="post thumbnail"
       />
-      <div className="post-item">
+      <Box className="post-item">
         <Typography paragraph variant="h4" className="post-title">
           <Link
             className={logged ? "has-text-primary" : "has-text-unlogin"}
@@ -30,11 +32,13 @@ const PostItem = ({post, logged}) => {
           >
             {post.frontmatter.title}
           </Link>
-          <span> &bull; </span>
-          <small>{post.frontmatter.date}</small>
+          <Typography component="span"> &bull; </Typography>
+          <Typography component="small">{post.frontmatter.date}</Typography>
         </Typography>
         <Typography paragraph>
-          <span className="post-excerpt">{post.excerpt}</span>
+          <Typography component="span" className="post-excerpt">
+            {post.excerpt}
+          </Typography>
           <Link
             className={logged ? "button is-small orange" : "button is-small"}
             to={logged ? post.fields.slug : "/login"}
@@ -42,9 +46,10 @@ const PostItem = ({post, logged}) => {
             Keep Reading →
           </Link>
         </Typography>
-      </div>
-    </div>
+      </Box>
+    </Box>
   )
 }
 
 export default PostItem
+export {PostItem}
