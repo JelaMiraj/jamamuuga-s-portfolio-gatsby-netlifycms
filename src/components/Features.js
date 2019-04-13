a319c482522a4ac9ca17aedae326cf596dc83f27
Jason Cady <jamamuuga@gmail.com>
2019-04-13 09:36:16 -0700

    Choose more appropriate components & add named export in Features.

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,23 +1,26 @@
 import React from "react"
 import PropTypes from "prop-types"
-import {Grid, Paper, Typography} from "@material-ui/core"
+import {CardContent, Grid, Paper, Typography} from "@material-ui/core"
 import {unstable_Box as Box} from "@material-ui/core/Box"
+import Card from "./Card"
 import PreviewCompatibleImage from "./PreviewCompatibleImage"
 
 const FeatureGrid = ({gridItems}) => (
   <Grid container wrap="wrap">
     {gridItems.map(item => (
       <Grid item xs={6} key={item.text}>
-        <Paper>
-          <section className="section">
+        <Card component="section">
+          <CardContent>
             <Typography component="div" paragraph align="center">
               <Box width="240px" display="inline-block">
                 <PreviewCompatibleImage imageInfo={item} />
               </Box>
             </Typography>
+          </CardContent>
+          <CardContent>
             <Typography paragraph>{item.text}</Typography>
-          </section>
-        </Paper>
+          </CardContent>
+        </Card>
       </Grid>
     ))}
   </Grid>
@@ -33,3 +36,4 @@ FeatureGrid.propTypes = {
 }
 
 export default FeatureGrid
+export {FeatureGrid as Features}
