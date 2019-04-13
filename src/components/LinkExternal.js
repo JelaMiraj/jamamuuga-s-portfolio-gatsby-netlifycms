07a7f8e005f69c567c9d5b180a103960d1a43f93
Jason Cady <jamamuuga@gmail.com>
2019-04-13 10:16:32 -0700

    Add separate component for external links.

---
+++ src/components/LinkExternal.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Link from "./Link"
+// TODO: Handle this using a custom property on regular custom Link component instead.
+
+function LinkExternal(props) {
+  return <Link {...props} target="_blank" rel="noopener noreferrer" />
+}
+
+export default LinkExternal
+export {LinkExternal}
