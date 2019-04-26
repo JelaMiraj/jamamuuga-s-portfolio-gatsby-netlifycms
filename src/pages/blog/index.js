571eb873dc55d67d275aa1daa70c723f14b5e3db
Jason Cady <jamamuuga@gmail.com>
2019-04-26 05:25:32 -0700

    Updated Blog page to match upstream & switched from class to function based react component

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,41 +7,37 @@ import {Typography} from "@material-ui/core"
 import {unstable_Box as Box} from "@material-ui/core/Box"
 import {BlogRoll, Layout} from "../../components"
 
-class BlogIndexPage extends Component {
-  render() {
-    return (
-      <Layout>
-        <Box component="section">
-          <Box px={[2, 3, 4]} mx="auto" maxWidth={1280} className="container">
-            <Box>
-              <Box
-                className="full-width-image-container margin-top-0"
-                style={{
-                  backgroundImage: `url("/img/blog-index.jpg")`,
-                }}
-              >
-                <Typography
-                  variant="h1"
-                  fontWeight="fontWeightBold"
-                  fontSize="fontSize1"
-                  style={{
-                    boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-                    backgroundColor: "#f40",
-                    color: "white",
-                    padding: "1rem",
-                  }}
-                >
-                  Latest Stories
-                </Typography>
-              </Box>
-            </Box>
-            <BlogRoll />
-          </Box>
+const BlogIndexPage = () => (
+  <Layout>
+    <Box
+      className="full-width-image-container margin-top-0"
+      style={{
+        backgroundImage: `url('/img/blog-index.jpg')`,
+      }}
+    >
+      <Typography
+        variant="h1"
+        fontWeight="fontWeightBold"
+        fontSize="fontSize1"
+        style={{
+          boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+          backgroundColor: "#f40",
+          color: "white",
+          padding: "1rem",
+        }}
+      >
+        Latest Stories
+      </Typography>
+    </Box>
+    <Box component="section" className="section">
+      <Box px={[2, 3, 4]} mx="auto" maxWidth={1280} className="container">
+        <Box className="content">
+          <BlogRoll />
         </Box>
-      </Layout>
-    )
-  }
-}
+      </Box>
+    </Box>
+  </Layout>
+)
 
 const BlogIndexPageWrapped = withRoot(BlogIndexPage)
 
