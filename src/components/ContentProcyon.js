00d28113f8fd6431db3298815b3fdb5469d33801
Jason Cady <jamamuuga@gmail.com>
2019-04-25 17:15:08 -0700

    Added a content component from Procyon.
    Use as inspiration for one of our own.

---
+++ src/components/ContentProcyon.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react"
+import Typography from "@material-ui/core"
+import styled from "@material-ui/styles"
+// Originally from: https://github.com/danielmahon/gatsby-starter-procyon/blob/netlifycms/src/components/Content.js
+
+const StyledTypography = styled(Typography)(theme => ({
+  "& img": {
+    maxWidth: "100%",
+  },
+}))
+
+class Content extends Component {
+  render() {
+    const {content, ...props} = this.props
+
+    // Set prop defaults
+    props.component = props.component || "div"
+
+    if (React.isValidElement(content)) {
+      return <StyledTypography {...props}>{content}</StyledTypography>
+    }
+
+    return (
+      <StyledTypography
+        {...props}
+        dangerouslySetInnerHTML={{__html: content}}
+      />
+    )
+  }
+}
+
+export default Content
