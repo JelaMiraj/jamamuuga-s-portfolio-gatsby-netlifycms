17646b5aada4d6ed57a558c1ad6f87a2c5c4f233
Jason Cady <jamamuuga@gmail.com>
2019-05-01 17:48:19 -0700

    Copy newer themes, with a safety first approach, from WIP branch.
    fontSize variables commented out in case current implementation of them causes errors.

--- src/styles/themes/theme-wip.js
+++ src/styles/themes/theme-wip.js
@@ -1,6 +1,9 @@
 import {colors as themeColors} from "./colors"
 // TODO: Make sure the final object has the correct structure and not something wierd like embedding an object in an object instead of assigning properties.
 
-export default {
+const Theme = {
   colors: themeColors,
 }
+
+export default Theme
+export {Theme as ThemeWIP}
