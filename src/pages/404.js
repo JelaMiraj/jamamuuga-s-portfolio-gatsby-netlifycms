1d28095002cd4999d4adcd05b8c27b3f4941e92a
Jason Cady <jamamuuga@gmail.com>
2019-05-09 01:47:10 -0700

    Changed Text in 404 Not Found page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
   <Layout title="Not Found">
     <div>
       <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist...</p>
+      <p>Alas, you just hit a page that doesn&#39;t exist...</p>
     </div>
   </Layout>
 )
