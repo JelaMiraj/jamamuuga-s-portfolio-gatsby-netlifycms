02b6b5bdd9992a5ff9b2bb55b01e29c31c15e030
Jason Cady <jamamuuga@gmail.com>
2019-05-16 20:57:09 -0700

    Simplify imports & fix code style in old index page from MUI Biz.

--- src/templates-not-using/index-page-MUIBiz.js
+++ src/templates-not-using/index-page-MUIBiz.js
@@ -1,63 +1,62 @@
-// import withRoot from "../utils/withRoot";
-// import React from "react";
-// import { graphql, Link } from "gatsby";
-// import PropTypes from "prop-types";
-// import SEO from "../components/SEO";
-// import Card from "../components/Card";
-// import Page from "../components/Page";
-// import HomeFeatures from "../components/HomeFeatures";
-// import Button from "@material-ui/core/Button";
-// import Carousel from "../components/Carousel";
-// import Avatar from "@material-ui/core/Avatar";
-// import { Gift } from "mdi-material-ui";
-// import withStyles from "@material-ui/styles/withStyles";
+// // eslint-disable-next-line
+// import {withRoot} from "../tools"
+// // eslint-disable-next-line
+// import React from "react"
+// import {graphql, Link} from "gatsby"
+// import PropTypes from "prop-types"
+// import {Avatar, Button} from "@material-ui/core"
+// import {Gift} from "mdi-material-ui"
+// import {withStyles} from "@material-ui/styles"
+// import {Card, Carousel, HomeFeatures, Page, SEO} from "../components"
 
 // const styles = theme => ({
 //     root: {
 //       fontWeight: "bold",
 //     },
-//   }),
-//   Home = props => {
-//     const products = props.data.allMarkdownRemark.edges;
-//     return (
-//       <Page title="Gatsby Material UI Business Starter">
-//         <SEO title="Home">
-//           <meta
-//             name="description"
-//             content="Beautiful Gatsby Material UI Business Starter. Tiny code. Well organized. Ready to customize and go."
-//           />
-//         </SEO>
+// })
 
-//         <HomeFeatures />
-//         <Card
-//           title="Our Products"
-//           avatar={
-//             <Avatar>
-//               <Gift />
-//             </Avatar>
-//           }
-//           action={
-//             <Button
-//               variant="contained"
-//               color="secondary"
-//               className={props.classes.root}
-//               component={Link}
-//               to="/products"
-//             >
-//               View All Products
-//             </Button>
-//           }
-//           style={{ minHeight: 523 }}
-//         >
-//           <Carousel items={products} />
-//         </Card>
-//       </Page>
-//     );
-//   };
+// const Home = props => {
+//   const products = props.data.allMarkdownRemark.edges
+
+//   return (
+//     <Page title="Gatsby Material UI Business Starter">
+//       <SEO title="Home">
+//         <meta
+//           name="description"
+//           content="Beautiful Gatsby Material UI Business Starter. Tiny code. Well organized. Ready to customize and go."
+//         />
+//       </SEO>
+
+//       <HomeFeatures />
+//       <Card
+//         title="Our Products"
+//         avatar={
+//           <Avatar>
+//             <Gift />
+//           </Avatar>
+//         }
+//         action={
+//           <Button
+//             variant="contained"
+//             color="secondary"
+//             className={props.classes.root}
+//             component={Link}
+//             to="/products"
+//           >
+//             View All Products
+//           </Button>
+//         }
+//         style={{ minHeight: 523 }}
+//       >
+//         <Carousel items={products} />
+//       </Card>
+//     </Page>
+//   )
+// }
 
 // export const query = graphql`
 //   query {
-//     allFile(filter: { extension: { eq: "jpg" } }) {
+//     allFile(filter: {extension: {eq: "jpg"}}) {
 //       edges {
 //         node {
 //           publicURL
@@ -65,8 +64,8 @@
 //       }
 //     }
 //     allMarkdownRemark(
-//       filter: { fileAbsolutePath: { regex: "/products/" } }
-//       sort: { fields: [frontmatter___date], order: DESC }
+//       filter: {fileAbsolutePath: {regex: "/products/"}}
+//       sort: {fields: [frontmatter___date], order: DESC}
 //     ) {
 //       edges {
 //         node {
@@ -84,10 +83,10 @@
 //       }
 //     }
 //   }
-// `;
+// `
 
 // Home.propTypes = {
 //   classes: PropTypes.object.isRequired,
-// };
+// }
 
-// export default withRoot(withStyles(styles)(Home));
+// export default withRoot(withStyles(styles)(Home))
