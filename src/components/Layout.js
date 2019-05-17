6d946248f5350495beb39a53ab8b1ed4095f804c
Jason Cady <jamamuuga@gmail.com>
2019-05-16 21:15:51 -0700

    Clean up & add external styles from MUI Biz to Layout component.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,59 +1,72 @@
-import React from "react"
-// FIXME: Switch to MUI Themes.
-// import {ThemeProvider} from "styled-components"
-// import {MUIThemeProvider, withStyles} from "@material-ui/core/styles"
+import React, {Component} from "react"
 import PropTypes from "prop-types"
 import {ScrollingProvider} from "react-scroll-section"
 import {Grid, Typography} from "@material-ui/core"
-// import GlobalStyle from "../styles/global-style"
-// TODO: Import a proper Styled System theme here instead of a separate pure color object.
-// import colors from "../styles/themes/colors"
+import {withStyles} from "@material-ui/styles"
+import GlobalStyle from "../styles/global-style"
 import Footer from "./Footer"
 import Header from "./Header"
 import Helmet from "./Helmet"
-// TODO: Why is this navigating down & back up to same folder?
 // import Navbar from "./Navbar"
-// import Footer from "../components/Footer-Huge-New"
-// import mytheme from "../styles/themes/theme"
-// FIXME: using MUIThemeProvider here breaks things...
+// import Footer from "./FooterHugeNew"
+
+const styles = {
+  container: {
+    marginTop: 94,
+  },
+  contentBox: {
+    maxWidth: "calc(1136px - 60px)",
+    width: "calc(100% - 60px)",
+  },
+  title: {
+    textAlign: "center",
+  },
+}
 
 // TODO: Find out if we need any <GlobalStyle />
 // const Layout = ({children}) => (
-const Layout = props => (
-  <>
-    {/* <MUIThemeProvider theme={mytheme}> */}
-    <ScrollingProvider>
-      <div>
-        <Helmet />
-        {/* <GlobalStyle /> */}
-        <Header />
-        {/* <div>{props.children}</div> */}
-        <Grid container style={{marginTop: "94px"}}>
-          <Grid item container direction="row">
-            <Grid item container direction="column" xs={12}>
-              {props.title ? (
-                <Typography
-                  variant="h2"
-                  gutterBottom
-                  style={{textAlign: "center"}}
-                >
-                  {props.title}
-                </Typography>
-              ) : null}
-              {props.children}
-              <Footer />
+class Layout extends Component {
+  render() {
+    const {classes, title, children} = this.props
+
+    return (
+      <>
+        <ScrollingProvider>
+          <div>
+            <Helmet />
+            {/* <GlobalStyle /> */}
+            <Header />
+            <Grid
+              className={classes.container}
+              container
+              direction="row"
+              justify="center"
+            >
+              <Grid className={classes.contentBox} item>
+                {title ? (
+                  <Typography
+                    className={classes.title}
+                    variant="h2"
+                    gutterBottom
+                  >
+                    {title}
+                  </Typography>
+                ) : null}
+                {children}
+                <Footer />
+              </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-      </div>
-    </ScrollingProvider>
-    {/* </MUIThemeProvider> */}
-  </>
-)
+          </div>
+        </ScrollingProvider>
+      </>
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+// export default Layout
+export default withStyles(styles)(Layout)
 // export {Layout}
