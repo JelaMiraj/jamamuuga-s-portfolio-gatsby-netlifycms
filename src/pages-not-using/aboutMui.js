41bb5d02f9bd94e59df9c00bf2f47fbf0b8b7711
Jason Cady <jamamuuga@gmail.com>
2019-06-01 04:13:13 -0700

    Add commented out & backed up About & Index pages from Material UI Gatsby Example.

---
+++ src/pages-not-using/aboutMui.js
@@ -0,0 +1,31 @@
+// import React from "react"
+// import {Box, Container, Link as MuiLink, Typography} from "@material-ui/core"
+// import {Link} from "../components"
+// import ProTip from "../components/ProTip"
+
+// function MadeWithLove() {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {"Built with love by the "}
+//       <MuiLink color="inherit" href="https://material-ui.com/">
+//         Material-UI
+//       </MuiLink>
+//       {" team."}
+//     </Typography>
+//   )
+// }
+
+// export default function App() {
+//   return (
+//     <Container maxWidth="sm">
+//       <Box my={4}>
+//         <Typography variant="h4" component="h1" gutterBottom>
+//           Gatsby v4-beta example
+//         </Typography>
+//         <Link to="/">Go to the main page</Link>
+//         <ProTip />
+//         <MadeWithLove />
+//       </Box>
+//     </Container>
+//   )
+// }
