513e58895a84c3221862fa4876a57e89721c498a
Jason Cady <jamamuuga@gmail.com>
2019-06-09 19:02:42 -0700

    Switch to new Footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
-import React from "react"
+import React, {Component} from "react"
 import {StaticQuery, graphql} from "gatsby"
-import {Divider, Hidden, Typography} from "@material-ui/core"
+import {Chip, Box, Button, Container, Divider, Grid, Hidden, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography} from "@material-ui/core"
 import {withStyles} from "@material-ui/core/styles"
+// import {Facebook as FacebookLogo, Instagram as InstagramLogo, Twitter as TwitterLogo, Vimeo as VimeoLogo} from "mdi-material-ui"
+import {Content, Link, LinkExternal, PreviewCompatibleImage as Image} from "."
 
 import logo from "../img/logo.svg"
 import facebook from "../img/social/facebook.svg"
@@ -15,8 +17,12 @@ const styles = theme => ({
     marginBottom: theme.spacing(3),
   },
   footer: {
+    // Also may need something like "footer has-background-black has-text-white-ter"
     marginBottom: theme.spacing(3),
     whiteSpace: "nowrap",
+  },
+  menuButton: {
+    color: theme.palette.primary.contrastText,
   },
 })
 
@@ -37,20 +43,98 @@ const Footer = withStyles(styles)(props => {
     <>
       <Divider className={classes.divider} />
       <footer className={classes.footer} id="footer">
-        <span>
-          <Typography variant="caption" component="span">
-            ©{new Date().getFullYear()} {title}{" "}
-            <Hidden only={["xs", "sm"]}>–</Hidden>
-            <Hidden only={["xl", "lg", "md"]}>
-              <br />
-            </Hidden>{" "}
-            {/* {email} – {phone} */}
-            <br />
-            &middot;
-            <br />
-            By <a href="https://jasoncadydesigns.netlify.com">Jason Cady</a>
-          </Typography>
-        </span>
+        <Box className="content has-text-centered">
+          <img src={logo} alt="Kaldi" style={{width: "14em", height: "10em"}} />
+        </Box>
+        <Box className="content has-text-centered has-background-black has-text-white-ter">
+          <Container className="container has-background-black has-text-white-ter">
+            <Grid container className="columns">
+              <Grid item xs={4} className="column is-4">
+                <section className="menu">
+                  <List component="nav" className="menu-list">
+                    <ListItem>
+                      <Link to="/" className="navbar-item">
+                        <ListItemText>Home</ListItemText>
+                      </Link>
+                    </ListItem>
+                    <ListItem>
+                      <Link className="navbar-item" to="/about">
+                        <ListItemText>About</ListItemText>
+                      </Link>
+                    </ListItem>
+                    <ListItem>
+                      <Link className="navbar-item" to="/products">
+                        <ListItemText>Products</ListItemText>
+                      </Link>
+                    </ListItem>
+                    <ListItem>
+                      <Link className="navbar-item" to="/contact/examples">
+                        <ListItemText>Form Examples</ListItemText>
+                      </Link>
+                    </ListItem>
+                    <ListItem>
+                      <LinkExternal
+                        className="navbar-item"
+                        href="/admin/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <ListItemText>Admin</ListItemText>
+                      </LinkExternal>
+                    </ListItem>
+                  </List>
+                </section>
+              </Grid>
+              <Grid item xs={4} className="column is-4">
+                <section>
+                  <List component="nav" className="menu-list">
+                    <ListItem>
+                      <Link className="navbar-item" to="/blog">
+                        <ListItemText>Latest Stories</ListItemText>
+                      </Link>
+                    </ListItem>
+                    <ListItem>
+                      <Link className="navbar-item" to="/contact">
+                        <ListItemText>Contact</ListItemText>
+                      </Link>
+                    </ListItem>
+                  </List>
+                </section>
+              </Grid>
+              <Grid item xs={4} className="column is-4 social">
+                <LinkExternal title="facebook" href="https://facebook.com">
+                  <img
+                    src={facebook}
+                    alt="Facebook"
+                    style={{width: "1em", height: "1em"}}
+                  />
+                </LinkExternal>
+                <LinkExternal title="twitter" href="https://twitter.com">
+                  <img
+                    className="fas fa-lg"
+                    src={twitter}
+                    alt="Twitter"
+                    style={{width: "1em", height: "1em"}}
+                  />
+                </LinkExternal>
+                <LinkExternal title="instagram" href="https://instagram.com">
+                  <img
+                    src={instagram}
+                    alt="Instagram"
+                    style={{width: "1em", height: "1em"}}
+                  />
+                </LinkExternal>
+                <LinkExternal title="vimeo" href="https://vimeo.com">
+                  <img
+                    src={vimeo}
+                    alt="Vimeo"
+                    style={{width: "1em", height: "1em"}}
+                  />
+                </LinkExternal>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
       </footer>
     </>
   )
@@ -74,3 +158,5 @@ export default props => (
     render={data => <Footer data={data} />}
   />
 )
+
+// export default Footer
---
+++ src/components/FooterOldMinimal.js
@@ -0,0 +1,76 @@
+// import React from "react"
+// import {StaticQuery, graphql} from "gatsby"
+// import {Divider, Hidden, Typography} from "@material-ui/core"
+// import {withStyles} from "@material-ui/core/styles"
+
+// import logo from "../img/logo.svg"
+// import facebook from "../img/social/facebook.svg"
+// import instagram from "../img/social/instagram.svg"
+// import twitter from "../img/social/twitter.svg"
+// import vimeo from "../img/social/vimeo.svg"
+
+// const styles = theme => ({
+//   divider: {
+//     marginTop: theme.spacing(6),
+//     marginBottom: theme.spacing(3),
+//   },
+//   footer: {
+//     marginBottom: theme.spacing(3),
+//     whiteSpace: "nowrap",
+//   },
+// })
+
+// const Footer = withStyles(styles)(props => {
+//   const {
+//     classes,
+//     data: {
+//       site: {
+//         siteMetadata: {
+//           title,
+//           contact: {email, phone},
+//         },
+//       },
+//     },
+//   } = props
+
+//   return (
+//     <>
+//       <Divider className={classes.divider} />
+//       <footer className={classes.footer} id="footer">
+//         <span>
+//           <Typography variant="caption" component="span">
+//             ©{new Date().getFullYear()} {title}{" "}
+//             <Hidden only={["xs", "sm"]}>–</Hidden>
+//             <Hidden only={["xl", "lg", "md"]}>
+//               <br />
+//             </Hidden>{" "}
+//             {/* {email} – {phone} */}
+//             <br />
+//             &middot;
+//             <br />
+//             By <a href="https://jasoncadydesigns.netlify.com">Jason Cady</a>
+//           </Typography>
+//         </span>
+//       </footer>
+//     </>
+//   )
+// })
+
+// export default props => (
+//   <StaticQuery
+//     query={graphql`
+//       query {
+//         site {
+//           siteMetadata {
+//             title
+//             contact {
+//               email
+//               phone
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={data => <Footer data={data} />}
+//   />
+// )
