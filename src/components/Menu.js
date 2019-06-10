8f9ea3e82faf3042a67e2ccc60fd432713e7349e
Jason Cady <jamamuuga@gmail.com>
2019-06-09 19:03:58 -0700

    Use Link External component in Menu component.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import {StaticQuery, graphql} from "gatsby"
 import {Button, IconButton} from "@material-ui/core"
 import {GithubCircle} from "mdi-material-ui"
 import {withStyles} from "@material-ui/core/styles"
-import {Link} from "."
+import {Link, LinkExternal} from "."
 // import logo from "../img/logo.svg"
 
 const styles = theme => ({
@@ -29,15 +29,13 @@ const Menu = withStyles(styles)(props => {
           <Button className={classes.menuButton}>{link.name}</Button>
         </Link>
       ))}
-      <a
+      <LinkExternal
         href="https://github.com/Jamamuuga/jamamuuga-s-portfolio-gatsby-netlifycms"
-        target="_blank"
-        rel="noopener noreferrer"
       >
         <IconButton className={classes.menuButton}>
           <GithubCircle />
         </IconButton>
-      </a>
+      </LinkExternal>
     </>
   )
 })
