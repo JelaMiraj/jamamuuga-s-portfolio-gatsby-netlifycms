6b368f5b9f7640da492465a4786c65feb4b9e299
Jason Cady <jamamuuga@gmail.com>
2019-07-04 23:45:33 -0700

    Change to function instead of classes in commented out old index page.

--- src/pages-not-using/indexOld.js
+++ src/pages-not-using/indexOld.js
@@ -7,46 +7,44 @@
 // // import {Card, Carousel} from "../components"
 // // FIXME: Somehow refer to color names from style variables instead of repeating here.
 
-// class IndexPage extends Component {
-//   render() {
-//     const {data} = this.props
-//     const {edges: posts} = data.allMarkdownRemark
+// const IndexPage = props => {
+//   const {data} = props
+//   const {edges: posts} = data.allMarkdownRemark
 
-//     return (
-//       <Layout>
-//         <section className="section">
-//           <div className="container">
-//             <div className="content">
-//               <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-//             </div>
-//             {posts.map(({node: post}) => (
-//               <div
-//                 className="content"
-//                 style={{border: "1px solid #333", padding: "2em 4em"}}
-//                 key={post.id}
-//               >
-//                 <p>
-//                   <Link className="has-text-primary" to={post.fields.slug}>
-//                     {post.frontmatter.title}
-//                   </Link>
-//                   <span> &bull; </span>
-//                   <small>{post.frontmatter.date}</small>
-//                 </p>
-//                 <p>
-//                   {post.excerpt}
-//                   <br />
-//                   <br />
-//                   <Link className="button is-small" to={post.fields.slug}>
-//                     Keep Reading →
-//                   </Link>
-//                 </p>
-//               </div>
-//             ))}
+//   return (
+//     <Layout>
+//       <section className="section">
+//         <div className="container">
+//           <div className="content">
+//             <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
 //           </div>
-//         </section>
-//       </Layout>
-//     )
-//   }
+//           {posts.map(({node: post}) => (
+//             <div
+//               className="content"
+//               style={{border: "1px solid #333", padding: "2em 4em"}}
+//               key={post.id}
+//             >
+//               <p>
+//                 <Link className="has-text-primary" to={post.fields.slug}>
+//                   {post.frontmatter.title}
+//                 </Link>
+//                 <span> &bull; </span>
+//                 <small>{post.frontmatter.date}</small>
+//               </p>
+//               <p>
+//                 {post.excerpt}
+//                 <br />
+//                 <br />
+//                 <Link className="button is-small" to={post.fields.slug}>
+//                   Keep Reading →
+//                 </Link>
+//               </p>
+//             </div>
+//           ))}
+//         </div>
+//       </section>
+//     </Layout>
+//   )
 // }
 
 // IndexPage.propTypes = {
