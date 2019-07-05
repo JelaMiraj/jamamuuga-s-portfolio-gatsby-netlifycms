be11f1c5680a31a9f4d91a93de5f5810d2440c0e
Jason Cady <jamamuuga@gmail.com>
2019-07-04 23:44:46 -0700

    Change to hooks api in Section component.

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,27 @@
 import {Grid} from "@material-ui/core"
-import {styled} from "@material-ui/core/styles"
+import {makeStyles} from "@material-ui/core/styles"
 // Originally from: https://github.com/danielmahon/gatsby-starter-procyon/blob/netlifycms/src/components/Section.js
 
-const Section = styled(Grid, {
-  component: "section",
-  spacing: 2,
-  justify: "center",
-  container: true,
-})(theme => ({
-  padding: "2em 0 4em 0",
-}))
+const useStyles = makeStyles({
+  grid: {
+    padding: "2em 0 4em 0",
+  },
+})
+
+const Section = props => {
+  const {children, classes} = props
+
+  return (
+    <Grid
+      component="section"
+      className={classes.grid}
+      spacing="2"
+      justify="center"
+      container="true"
+    >
+      {children}
+    </Grid>
+  )
+}
 
 export default Section
