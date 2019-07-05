6fa7952f4aa1d05aca7905bbf3a96bd5fadb4713
Jason Cady <jamamuuga@gmail.com>
2019-07-04 23:51:39 -0700

    Use Material UI Components and switch to hooks api in Blog Index page.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,38 +1,46 @@
-import React, {Component} from "react"
-import {BlogRoll, Layout} from "../../components"
+import React from "react"
+import {Container, Typography} from "@material-ui/core"
+import {makeStyles} from "@material-ui/core/styles"
+import {BlogRoll, Content, Layout} from "../../components"
 
-class BlogIndexPage extends Component {
-  render() {
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <div
-                className="full-width-image-container margin-top-0"
-                style={{
-                  backgroundImage: `url("/img/blog-index.jpg")`,
-                }}
-              >
-                <h1
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-                    backgroundColor: "#f40",
-                    color: "white",
-                    padding: "1rem",
-                  }}
-                >
-                  Latest Stories
-                </h1>
-              </div>
-            </div>
-            <BlogRoll />
-          </div>
-        </section>
-      </Layout>
-    )
+const useStyles = makeStyles({
+  typography: {
+    boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+    backgroundColor: "#f40",
+    color: "white",
+    padding: "1rem",
   }
+})
+
+const BlogIndexPage = props => {
+  const classes = useStyles(props)
+
+  return (
+    <Layout>
+      <section className="section">
+        <Container className={classes.container}>
+          <div className="content">
+            <div
+              className="full-width-image-container margin-top-0"
+              style={{
+                backgroundImage: `url("/img/blog-index.jpg")`,
+              }}
+            >
+              <Typography
+                variant="h1"
+                className={classes.typography}
+                fontWeight="fontWeightBold"
+              >
+                {/*                 className="is-size-1" */}
+                Latest Stories
+            </Typography>
+            </div>
+          </div>
+          <BlogRoll />
+        </Container>
+      </section>
+    </Layout>
+  )
 }
 
 export default BlogIndexPage
