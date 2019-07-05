bfaa3b9a9df324f73f0d0847a1def55c81d33ce3
Jason Cady <jamamuuga@gmail.com>
2019-07-04 23:41:55 -0700

    Change to hooks api with functions instead of classes in commented out old Navbar components.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,108 +1,105 @@
-import React, {Component} from "react"
-import {Button, Container, IconButton} from "@material-ui/core"
-import {GithubCircle} from "mdi-material-ui"
-import Link from "./Link"
-// import {Heading, Image, Text} from "rebass"
-import github from "../img/github-icon.svg"
-import logo from "../img/logo.svg"
-// TODO: Convert to extending or using Material UI's Navbar.
-import NavbarStyle from "../styles/components/navbar-style"
+// import React from "react"
+// import {Button, Container, IconButton} from "@material-ui/core"
+// import {GithubCircle} from "mdi-material-ui"
+// // TODO: Replace "a" anchor links with LinkExternal.
+// import {Link, LinkExternal} from "."
+// import logo from "../img/logo.svg"
+// // TODO: Convert to extending or using Material UI's Navbar.
+// import NavbarStyle from "../styles/components/navbar-style"
+// // TODO: Port anything you want to keep then get rid of this file.
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: "",
-    }
-  }
+// const Navbar = props => {
+//   // FIXME: Need to find alternative to setState method, because probably no more "this." in local function instead of method.
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: "is-active",
-            })
-          : this.setState({
-              navBarActiveClass: "",
-            })
-      },
-    )
-  }
+//   const state = {
+//     active: false,
+//     navBarActiveClass: "",
+//   }
 
-  render() {
-    return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{width: "88px"}} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/portfolio">
-                Portfolio
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/Jamamuuga/jamamuuga-s-portfolio-gatsby-netlifycms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {/*
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-                */}
-                <IconButton className="icon" style={{color: "#fff"}}>
-                  <GithubCircle />
-                </IconButton>
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    )
-  }
-}
+//   const toggleHamburger = () => {
+//     // toggle the active boolean in the state
+//     this.setState(
+//       {
+//         active: !this.state.active,
+//       },
+//       // after state has been updated,
+//       () => {
+//         // set the class in state for the navbar accordingly
+//         this.state.active
+//           ? this.setState({
+//               navBarActiveClass: "is-active",
+//             })
+//           : this.setState({
+//               navBarActiveClass: "",
+//             })
+//       },
+//     )
+//   }
 
-export default Navbar
+//   return (
+//     <nav
+//       className="navbar is-transparent"
+//       role="navigation"
+//       aria-label="main-navigation"
+//     >
+//       <div className="container">
+//         <div className="navbar-brand">
+//           <Link to="/" className="navbar-item" title="Logo">
+//             <img src={logo} alt="Kaldi" style={{width: "88px"}} />
+//           </Link>
+//           {/* Hamburger menu */}
+//           <div
+//             className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+//             data-target="navMenu"
+//             onClick={() => this.toggleHamburger()}
+//           >
+//             <span />
+//             <span />
+//             <span />
+//           </div>
+//         </div>
+//         <div
+//           id="navMenu"
+//           className={`navbar-menu ${this.state.navBarActiveClass}`}
+//         >
+//           <div className="navbar-start has-text-centered">
+//             <Link className="navbar-item" to="/about">
+//               About
+//             </Link>
+//             <Link className="navbar-item" to="/portfolio">
+//               Portfolio
+//             </Link>
+//             <Link className="navbar-item" to="/blog">
+//               Blog
+//             </Link>
+//             <Link className="navbar-item" to="/contact">
+//               Contact
+//             </Link>
+//             <Link className="navbar-item" to="/contact/examples">
+//               Form Examples
+//             </Link>
+//           </div>
+//           <div className="navbar-end has-text-centered">
+//             <a
+//               className="navbar-item"
+//               href="https://github.com/Jamamuuga/jamamuuga-s-portfolio-gatsby-netlifycms"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               {/*
+//               <span className="icon">
+//                 <img src={github} alt="Github" />
+//               </span>
+//               */}
+//               <IconButton className="icon" style={{color: "#fff"}}>
+//                 <GithubCircle />
+//               </IconButton>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   )
+// }
+
+// export default Navbar
--- src/components/NavbarBaseNew.js
+++ src/components/NavbarBaseNew.js
@@ -1,18 +1,20 @@
 // import React from "react"
-// import {Link} from "."
-// import github from "../img/github-icon.svg"
+// import {Button, Container, IconButton} from "@material-ui/core"
+// import {GithubCircle} from "mdi-material-ui"
+// // TODO: Replace "a" anchor links with LinkExternal.
+// import {Link, LinkExternal} from "."
 // import logo from "../img/logo.svg"
+// // TODO: Port anything you want to keep then get rid of this file.
 
-// const Navbar = class extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
+// const Navbar = props => {
+//   // FIXME: Need to find alternative to setState method, because probably no more "this." in local function instead of method.
+
+//   const state = {
 //       active: false,
 //       navBarActiveClass: "",
 //     }
-//   }
 
-//   toggleHamburger = () => {
+//   const toggleHamburger = () => {
 //     // toggle the active boolean in the state
 //     this.setState(
 //       {
@@ -32,67 +34,69 @@
 //     )
 //   }
 
-//   render() {
-//     return (
-//       <nav
-//         className="navbar is-transparent"
-//         role="navigation"
-//         aria-label="main-navigation"
-//       >
-//         <div className="container">
-//           <div className="navbar-brand">
-//             <Link to="/" className="navbar-item" title="Logo">
-//               <img src={logo} alt="Kaldi" style={{width: "88px"}} />
-//             </Link>
-//             {/* Hamburger menu */}
-//             <div
-//               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-//               data-target="navMenu"
-//               onClick={() => this.toggleHamburger()}
-//             >
-//               <span />
-//               <span />
-//               <span />
-//             </div>
-//           </div>
+//   return (
+//     <nav
+//       className="navbar is-transparent"
+//       role="navigation"
+//       aria-label="main-navigation"
+//     >
+//       <div className="container">
+//         <div className="navbar-brand">
+//           <Link to="/" className="navbar-item" title="Logo">
+//             <img src={logo} alt="Kaldi" style={{width: "88px"}} />
+//           </Link>
+//           {/* Hamburger menu */}
 //           <div
-//             id="navMenu"
-//             className={`navbar-menu ${this.state.navBarActiveClass}`}
+//             className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+//             data-target="navMenu"
+//             onClick={() => this.toggleHamburger()}
 //           >
-//             <div className="navbar-start has-text-centered">
-//               <Link className="navbar-item" to="/about">
-//                 About
-//               </Link>
-//               <Link className="navbar-item" to="/portfolio">
-//                 Portfolio
-//               </Link>
-//               <Link className="navbar-item" to="/blog">
-//                 Blog
-//               </Link>
-//               <Link className="navbar-item" to="/contact">
-//                 Contact
-//               </Link>
-//               <Link className="navbar-item" to="/contact/examples">
-//                 Form Examples
-//               </Link>
-//             </div>
-//             <div className="navbar-end has-text-centered">
-//               <a
-//                 className="navbar-item"
-//                 href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <span className="icon">
-//                   <img src={github} alt="Github" />
-//                 </span>
-//               </a>
-//             </div>
+//             <span />
+//             <span />
+//             <span />
 //           </div>
 //         </div>
-//       </nav>
-//     )
-//   }
+//         <div
+//           id="navMenu"
+//           className={`navbar-menu ${this.state.navBarActiveClass}`}
+//         >
+//           <div className="navbar-start has-text-centered">
+//             <Link className="navbar-item" to="/about">
+//               About
+//             </Link>
+//             <Link className="navbar-item" to="/portfolio">
+//               Portfolio
+//             </Link>
+//             <Link className="navbar-item" to="/blog">
+//               Blog
+//             </Link>
+//             <Link className="navbar-item" to="/contact">
+//               Contact
+//             </Link>
+//             <Link className="navbar-item" to="/contact/examples">
+//               Form Examples
+//             </Link>
+//           </div>
+//           <div className="navbar-end has-text-centered">
+//             <a
+//               className="navbar-item"
+//               href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               {/* <span className="icon">
+//                 <img src={github} alt="Github" />
+//                 </span> */}
+//                             <IconButton className="icon" style={{color: "#fff"}}>
+//                 <GithubCircle />
+//               </IconButton>
+
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   )
 // }
 
 // export default Navbar
