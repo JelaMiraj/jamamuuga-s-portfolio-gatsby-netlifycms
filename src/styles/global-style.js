94c370ca4aa52d309e40c0fd67659cdda26a14a0
Jason Cady <jamamuuga@gmail.com>
2019-07-04 23:53:45 -0700

    Remove old commented out stuff from Global Style.

--- src/styles/global-style.js
+++ src/styles/global-style.js
@@ -1,17 +1,8 @@
 import React from "react"
 import {CssBaseline} from "@material-ui/core"
-// import styled, {createGlobalStyle} from "styled-components"
-// import reboot from "styled-reboot"
-// TODO: Import a proper Styled System theme here instead of a separate pure color object.
-// import colors from "./themes/colors"
 
 const options = {}
 
-// const rebootCss = reboot(/* options */)
-
-// const GlobalStyle = createGlobalStyle`
-//   ${rebootCss}
-// `
 const GlobalStyle = () => (
   <>
     <CssBaseline />
