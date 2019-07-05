1fa657e3b744888389771bd994863e852fddc67d
Jason Cady <jamamuuga@gmail.com>
2019-07-04 23:37:21 -0700

    Comment out mid transition Carousel component.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,126 +1,135 @@
-import React, {Component} from "react"
-import {withPrefix} from "gatsby"
-import {
-  CardContent,
-  CardMedia,
-  Button,
-  MobileStepper,
-  Paper,
-  Typography,
-} from "@material-ui/core"
-import SwipeableViews from "react-swipeable-views"
-import {autoPlay} from "react-swipeable-views-utils"
-import {withStyles} from "@material-ui/core/styles"
-import {Card, Link} from "."
-// TODO: Convert from class & withStyle to function constant & makeStyles hooks api. Also this means styles should be changed to a function constant names useStyles.
+// import React from "react"
+// import {withPrefix} from "gatsby"
+// import {
+//   CardContent,
+//   CardMedia,
+//   Button,
+//   MobileStepper,
+//   Paper,
+//   Typography,
+// } from "@material-ui/core"
+// import {withStyles} from "@material-ui/core/styles"
+// import {
+//   usePopupState,
+//   bindTrigger,
+//   bindMenu,
+// } from "material-ui-popup-state/hooks"
+// import SwipeableViews from "react-swipeable-views"
+// import {autoPlay} from "react-swipeable-views-utils"
+// import {Card, Link} from "."
+// // Make sure to use Material UI's Steppers & not just material-ui-popup-state: https://material-ui.com/components/steppers/#mobile-stepper-text-with-carousel-effect
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
-const styles = {
-  cardMedia: {
-    height: "200px",
-  },
-}
+// const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
-class Carousel extends Component {
-  state = {
-    activeStep: 0,
-  }
+// const useStyles = makeStyles({
+//   cardMedia: {
+//     height: "200px",
+//   },
+// })
 
-  handleNext = () => {
-    this.setState(prevState => ({
-      activeStep: prevState.activeStep + 1,
-    }))
-  }
+// const Carousel = props => {
+//   // TODO: Find alternative to this.setState.
+//   // FIXME: Finish porting to function style, material-ui-popup-state/hooks, & Material UI Steppers.
 
-  handleBack = () => {
-    this.setState(prevState => ({
-      activeStep: prevState.activeStep - 1,
-    }))
-  }
+//   const popupState = usePopupState({})
 
-  handleStepChange = activeStep => {
-    this.setState({activeStep})
-  }
+//   const state = {
+//     activeStep: 0,
+//   }
 
-  render() {
-    const {activeStep} = this.state
+//   const handleNext = () => {
+//     this.setState(prevState => ({
+//       activeStep: prevState.activeStep + 1,
+//     }))
+//   }
 
-    const {classes, items} = this.props
+//   const handleBack = () => {
+//     this.setState(prevState => ({
+//       activeStep: prevState.activeStep - 1,
+//     }))
+//   }
 
-    const maxSteps = items.length
+//   const handleStepChange = activeStep => {
+//     this.setState({activeStep})
+//   }
 
-    return (
-      <Paper elevation={0}>
-        <div>
-          <Typography>{items[activeStep].title}</Typography>
-          <AutoPlaySwipeableViews
-            axis="x"
-            index={activeStep}
-            onChangeIndex={this.handleStepChange}
-            enableMouseEvents
-          >
-            {items.map((item, index) => {
-              const {
-                node: {
-                  excerpt,
-                  frontmatter: {
-                    path,
-                    title,
-                    image: {publicURL},
-                  },
-                },
-              } = item
+//   const {activeStep} = state
 
-              return (
-                <div key={index}>
-                  {Math.abs(activeStep - index) <= 2 ? (
-                    <Card>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image={withPrefix(publicURL)}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          <Link to={path}>{title}</Link>
-                        </Typography>
-                        <Typography component="p">{excerpt}</Typography>
-                      </CardContent>
-                    </Card>
-                  ) : null}
-                </div>
-              )
-            })}
-          </AutoPlaySwipeableViews>
-          <MobileStepper
-            steps={maxSteps}
-            position="static"
-            activeStep={activeStep}
-            nextButton={
-              <Button
-                size="small"
-                onClick={this.handleNext}
-                disabled={activeStep === maxSteps - 1}
-              >
-                Next
-              </Button>
-            }
-            backButton={
-              <Button
-                size="small"
-                onClick={this.handleBack}
-                disabled={activeStep === 0}
-              >
-                Back
-              </Button>
-            }
-          />
-        </div>
-      </Paper>
-    )
-  }
-}
+//   const classes = useStyles(props)
+//   const {items} = props
+
+//   const maxSteps = items.length
+
+//   return (
+//     <Paper elevation={0}>
+//       <div>
+//         <Typography>{items[activeStep].title}</Typography>
+//         <AutoPlaySwipeableViews
+//           axis="x"
+//           index={activeStep}
+//           onChangeIndex={this.handleStepChange}
+//           enableMouseEvents
+//         >
+//           {items.map((item, index) => {
+//             const {
+//               node: {
+//                 excerpt,
+//                 frontmatter: {
+//                   path,
+//                   title,
+//                   image: {publicURL},
+//                 },
+//               },
+//             } = item
+
+//             return (
+//               <div key={index}>
+//                 {Math.abs(activeStep - index) <= 2 ? (
+//                   <Card>
+//                     <CardMedia
+//                       className={classes.cardMedia}
+//                       image={withPrefix(publicURL)}
+//                     />
+//                     <CardContent>
+//                       <Typography gutterBottom variant="h5" component="h2">
+//                         <Link to={path}>{title}</Link>
+//                       </Typography>
+//                       <Typography component="p">{excerpt}</Typography>
+//                     </CardContent>
+//                   </Card>
+//                 ) : null}
+//               </div>
+//             )
+//           })}
+//         </AutoPlaySwipeableViews>
+//         <MobileStepper
+//           steps={maxSteps}
+//           position="static"
+//           activeStep={activeStep}
+//           nextButton={
+//             <Button
+//               size="small"
+//               onClick={this.handleNext}
+//               disabled={activeStep === maxSteps - 1}
+//             >
+//               Next
+//             </Button>
+//           }
+//           backButton={
+//             <Button
+//               size="small"
+//               onClick={this.handleBack}
+//               disabled={activeStep === 0}
+//             >
+//               Back
+//             </Button>
+//           }
+//         />
+//       </div>
+//     </Paper>
+//   )
+// }
 
 // export default Carousel
-export default withStyles(styles)(Carousel)
-// export {Carousel}
+// // export {Carousel}
