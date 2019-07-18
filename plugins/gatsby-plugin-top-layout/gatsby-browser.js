0c06bfa7de810343d19a2cf1fa7fb20e174defbc
Jason Cady <jamamuuga@gmail.com>
2019-07-18 13:08:11 -0700

    Switch to explicit import of TopLayout internally.

--- plugins/gatsby-plugin-top-layout/gatsby-browser.js
+++ plugins/gatsby-plugin-top-layout/gatsby-browser.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export, react/prop-types */
 
 import React from "react"
-import TopLayout from "./TopLayout"
+import {TopLayout} from "."
 
 export const wrapRootElement = ({element}) => <TopLayout>{element}</TopLayout>
