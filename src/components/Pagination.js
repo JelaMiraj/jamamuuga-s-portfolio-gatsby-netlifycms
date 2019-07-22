9954131833b59157678c1483e6211a3b1f272beb
Jason Cady <jamamuuga@gmail.com>
2019-07-22 15:43:44 -0700

    Use Material UI in Pagination.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,15 +1,29 @@
 import React from "react"
 import {List, ListItem, ListItemText} from "@material-ui/core"
+import {makeStyles} from "@material-ui/core/styles"
 import {Link} from "."
 // Pagination for posts.
 
-const unorderedListStyle = {
-  display: "flex",
-  flexWrap: "wrap",
-  justifyContent: "space-between",
-  alignItems: "center",
-  listStyle: "none",
-}
+// TODO: Convert styling to Material UI Styles
+const useStyles = makeStyles({
+  list: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems: "center",
+    listStyle: "none",
+  },
+  // link: {
+  // },
+})
+
+// const unorderedListStyle = {
+//   display: "flex",
+//   flexWrap: "wrap",
+//   justifyContent: "space-between",
+//   alignItems: "center",
+//   listStyle: "none",
+// }
 
 const linkStyle = (i, currentPage) => ({
   padding: 10,
@@ -19,20 +33,22 @@ const linkStyle = (i, currentPage) => ({
 })
 
 const Pagination = ({currentPage, numPages}) => {
+  const classes = useStyles()
   const isFirst = currentPage === 1
   const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
   const isLast = currentPage === numPages
   const nextPage = (currentPage + 1).toString()
 
+  //    {/* <List style={unorderedListStyle}> */}
   return (
-    <ul style={unorderedListStyle}>
+    <List className={classes.list}>
       {!isFirst && (
         <Link to={prevPage} rel="prev" className="has-text-primary">
           ← Previous Page
         </Link>
       )}
       {Array.from({length: numPages}, (_, i) => (
-        <li
+        <ListItem
           key={`pagination-number${i + 1}`}
           style={{
             margin: 0,
@@ -44,14 +60,14 @@ const Pagination = ({currentPage, numPages}) => {
           >
             {i + 1}
           </Link>
-        </li>
+        </ListItem>
       ))}
       {!isLast && (
         <Link to={nextPage} rel="next" className="has-text-primary">
           Next Page →
         </Link>
       )}
-    </ul>
+    </List>
   )
 }
 
