a7bba2ab9b8e43301625c240fc87ecc8a5a663ec
Jason Cady <jamamuuga@gmail.com>
2019-07-22 14:36:56 -0700

    Change code style & added comment to Tags Page.
    Using the same style of function header as upstream helps avoid confusing git diff.

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -4,42 +4,43 @@ import {kebabCase} from "lodash"
 import {graphql} from "gatsby"
 // import {Helmet, Layout, Link} from "../../components"
 import {Layout, Link} from "../../components"
+// TODO: Use custom helmet instead of React Helmet directly.
 
-export default function TagsPage({
+const TagsPage = ({
   data: {
     allMarkdownRemark: {group},
     site: {
       siteMetadata: {title},
     },
   },
-}) {
-  return (
-    <Layout>
-      <section className="section">
-        <Helmet title={`Tags | ${title}`} />
-        <div className="container content">
-          <div className="columns">
-            <div
-              className="column is-10 is-offset-1"
-              style={{marginBottom: "6rem"}}
-            >
-              <h1 className="title is-size-2 is-bold-light">Tags</h1>
-              <ul className="taglist">
-                {group.map(tag => (
-                  <li key={tag.fieldValue}>
-                    <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                      {tag.fieldValue} ({tag.totalCount})
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
+}) => (
+  <Layout>
+    <section className="section">
+      <Helmet title={`Tags | ${title}`} />
+      <div className="container content">
+        <div className="columns">
+          <div
+            className="column is-10 is-offset-1"
+            style={{marginBottom: "6rem"}}
+          >
+            <h1 className="title is-size-2 is-bold-light">Tags</h1>
+            <ul className="taglist">
+              {group.map(tag => (
+                <li key={tag.fieldValue}>
+                  <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                    {tag.fieldValue} ({tag.totalCount})
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      </section>
-    </Layout>
-  )
-}
+      </div>
+    </section>
+  </Layout>
+)
+
+export default TagsPage
 
 export const tagPageQuery = graphql`
   query TagsQuery {
