a171554efdece19ab95d259738e71eb6c0e3274b
Jason Cady <jamamuuga@gmail.com>
2019-07-23 18:53:35 -0700

    Use current custom Helmet in not using General templat.

--- src/templates-not-using/general.js
+++ src/templates-not-using/general.js
@@ -3,7 +3,7 @@
 // import {graphql, withPrefix} from "gatsby"
 // import {CardContent, CardMedia, Typography} from "@material-ui/core"
 // import {makeStyles} from "@material-ui/core/styles"
-// import {Card, Layout} from "../components"
+// import {Card, Helmet, Layout} from "../components"
 
 // const useStyles = makeStyles({
 //   cardMedia: {
@@ -21,7 +21,7 @@
 
 //   return (
 //     <Layout>
-//       {/* <SEO title={title} /> */}
+//       <Helmet title={title} />
 //       <Card>
 //         <CardMedia
 //           className={classes.cardMedia}
