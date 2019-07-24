67eb5d3c6225a9a50d922f2f46a290376b157c2a
Jason Cady <jamamuuga@gmail.com>
2019-07-23 19:04:07 -0700

    Fix missing close bracket & Auto-format Pricing component.

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -5,49 +5,44 @@ import {makeStyles} from "@material-ui/core/styles"
 // TODO: Maybe use Card and/or Paper somewhere in Pricing.
 
 const useStyles = makeStyles({
-  sectionBox: {
-
-  },
+  sectionBox: {},
 })
 
 const Pricing = ({data}) => {
   const classes = useStyles()
 
   return (
-  <Grid container>
-    {data.map(price => (
-      <Grid item xs={4} key={price.plan}>
-        <Box component="section" className={classes.sectionBox}>
-          <Typography
-            variant="h4"
-            align="center"
-            fontWeight="600"
-          >
-            {price.plan}
-          </Typography>
-          <Typography
-            variant="h2"
-            fontWeight="fontWeightBold"
-            color="primary"
-            align="center"
-          >
-            ${price.price}
-          </Typography>
-          <Typography paragraph={true} fontWeight="600">
-            {price.description}
-          </Typography>
-          <List>
-            {price.items.map(item => (
-              <ListItem key={item} xs={5}>
-                {item}
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Grid>
-    ))}
-  </Grid>
-)
+    <Grid container>
+      {data.map(price => (
+        <Grid item xs={4} key={price.plan}>
+          <Box component="section" className={classes.sectionBox}>
+            <Typography variant="h4" align="center" fontWeight="600">
+              {price.plan}
+            </Typography>
+            <Typography
+              variant="h2"
+              fontWeight="fontWeightBold"
+              color="primary"
+              align="center"
+            >
+              ${price.price}
+            </Typography>
+            <Typography paragraph fontWeight="600">
+              {price.description}
+            </Typography>
+            <List>
+              {price.items.map(item => (
+                <ListItem key={item} xs={5}>
+                  {item}
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
 
 Pricing.propTypes = {
   data: PropTypes.arrayOf(
