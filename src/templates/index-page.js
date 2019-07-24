55ee5804c00d9024da37d7749ffa175877a41d01
Jason Cady <jamamuuga@gmail.com>
2019-07-23 19:06:08 -0700

    Alphabetize imports in Index Page temmplate.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,10 +3,10 @@ import {PropTypes} from "prop-types"
 import {graphql} from "gatsby"
 import {
   Box,
+  Button,
   CardContent,
   CardMedia,
   Container,
-  Button,
   Grid,
   Paper,
   Typography,
