70618c5ab291d52c363f7ea1de056f82e5369904
Jason Cady <jamamuuga@gmail.com>
2019-07-23 18:47:49 -0700

    Convert more Bulma to Material UI in Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,14 +26,24 @@ import twitter from "../img/social/twitter.svg"
 import vimeo from "../img/social/vimeo.svg"
 
 const useStyles = makeStyles({
+  contentGridBox: {
+    align: "center",
+    backgroundColor:"black",
+    color: "hsl(0, 0%, 96%)",
+  },
   divider: {
     marginTop: "6",
     marginBottom: "3",
   },
   footer: {
-    // Also may need something like "footer has-background-black has-text-white-ter"
+    backgroundColor:"black",
+    color: "hsl(0, 0%, 96%)",
     marginBottom: "3",
     whiteSpace: "nowrap",
+  },
+  footerNavContainer: {
+    backgroundColor:"black",
+    color: "hsl(0, 0%, 96%)",
   },
   menuButton: {
     color: "primary.contrastText",
@@ -58,14 +68,14 @@ const Footer = props => {
     <>
       <Divider className={classes.divider} />
       <footer className={classes.footer} id="footer">
-        <Box className="content has-text-centered">
+        <Box align="center" className="content">
           <img src={logo} alt="Kaldi" style={{width: "14em", height: "10em"}} />
         </Box>
-        <Box className="content has-text-centered has-background-black has-text-white-ter">
-          <Container className="container has-background-black has-text-white-ter">
-            <Grid container className="columns">
+        <Box className={classes.contentGridBox}>
+          <Container className={classes.footerNavContainer}>
+            <Grid container>
               <Grid item xs={4}>
-                <section className="menu">
+                <Box component="section" className="menu">
                   <List component="nav" className="menu-list">
                     <ListItem>
                       <Link to="/" className="navbar-item">
@@ -98,10 +108,10 @@ const Footer = props => {
                       </LinkExternal>
                     </ListItem>
                   </List>
-                </section>
+                </Box>
               </Grid>
               <Grid item xs={4}>
-                <section>
+                <Box component="section">
                   <List component="nav" className="menu-list">
                     {/* <ListItem>
                       <Link className="navbar-item" to="/blog">
@@ -114,7 +124,7 @@ const Footer = props => {
                       </Link>
                     </ListItem>
                   </List>
-                </section>
+                </Box>
               </Grid>
               <Grid item xs={4} className="social">
                 <LinkExternal title="facebook" href="https://facebook.com">
