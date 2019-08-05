a6913790a627f75c485f35e8eff6d2c84c7a7217
Jason Cady <jamamuuga@gmail.com>
2019-08-05 15:05:57 -0700

    Comment out import transforming for now in Babel config.

--- .babelrc.js
+++ .babelrc.js
@@ -15,39 +15,39 @@ module.exports = {
     //   },
     // ],
     // "babel-plugin-add-module-exports",
-    [
-      "babel-plugin-transform-imports",
-      {
-        lodash: {
-          transform: "lodash/${member}",
-          preventFullImport: true,
-        },
-        "@material-ui/core": {
-          // transform: "@material-ui/core/esm/${member}",
-          // for bundlers not supporting ES modules use:
-          transform: "@material-ui/core/${member}",
-          preventFullImport: true,
-        },
-        "@material-ui/icons": {
-          // transform: "@material-ui/icons/esm/${member}",
-          // for bundlers not supporting ES modules use:
-          transform: "@material-ui/icons/${member}",
-          preventFullImport: true,
-        },
-        "@material-ui/styles": {
-          // transform: "@material-ui/styles/esm/${member}",
-          // for bundlers not supporting ES modules use:
-          transform: "@material-ui/styles/${member}",
-          preventFullImport: true,
-        },
-        "@material-ui/system": {
-          // transform: "@material-ui/system/esm/${member}",
-          // for bundlers not supporting ES modules use:
-          transform: "@material-ui/system/${member}",
-          preventFullImport: true,
-        },
-      },
-    ],
+    // [
+    //   "babel-plugin-transform-imports",
+    //   {
+    //     lodash: {
+    //       transform: "lodash/${member}",
+    //       preventFullImport: true,
+    //     },
+    //     "@material-ui/core": {
+    //       // transform: "@material-ui/core/esm/${member}",
+    //       // for bundlers not supporting ES modules use:
+    //       transform: "@material-ui/core/${member}",
+    //       preventFullImport: true,
+    //     },
+    //     "@material-ui/icons": {
+    //       // transform: "@material-ui/icons/esm/${member}",
+    //       // for bundlers not supporting ES modules use:
+    //       transform: "@material-ui/icons/${member}",
+    //       preventFullImport: true,
+    //     },
+    //     "@material-ui/styles": {
+    //       // transform: "@material-ui/styles/esm/${member}",
+    //       // for bundlers not supporting ES modules use:
+    //       transform: "@material-ui/styles/${member}",
+    //       preventFullImport: true,
+    //     },
+    //     "@material-ui/system": {
+    //       // transform: "@material-ui/system/esm/${member}",
+    //       // for bundlers not supporting ES modules use:
+    //       transform: "@material-ui/system/${member}",
+    //       preventFullImport: true,
+    //     },
+    //   },
+    // ],
     "babel-plugin-styled-components",
   ], // .map(require.resolve),
 }
