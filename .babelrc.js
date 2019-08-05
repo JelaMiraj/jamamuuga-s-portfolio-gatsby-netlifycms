1be93fd209a9e98b6d33c99491c4482395039297
Jason Cady <jamamuuga@gmail.com>
2019-08-05 13:03:27 -0700

    Try alt file path for import transforms in Babel config.

--- .babelrc.js
+++ .babelrc.js
@@ -23,27 +23,27 @@ module.exports = {
           preventFullImport: true,
         },
         "@material-ui/core": {
-          transform: "@material-ui/core/esm/${member}",
+          // transform: "@material-ui/core/esm/${member}",
           // for bundlers not supporting ES modules use:
-          // transform: "@material-ui/core/${member}",
+          transform: "@material-ui/core/${member}",
           preventFullImport: true,
         },
         "@material-ui/icons": {
-          transform: "@material-ui/icons/esm/${member}",
+          // transform: "@material-ui/icons/esm/${member}",
           // for bundlers not supporting ES modules use:
-          // transform: "@material-ui/icons/${member}",
+          transform: "@material-ui/icons/${member}",
           preventFullImport: true,
         },
         "@material-ui/styles": {
-          transform: "@material-ui/styles/esm/${member}",
+          // transform: "@material-ui/styles/esm/${member}",
           // for bundlers not supporting ES modules use:
-          // transform: "@material-ui/styles/${member}",
+          transform: "@material-ui/styles/${member}",
           preventFullImport: true,
         },
         "@material-ui/system": {
-          transform: "@material-ui/system/esm/${member}",
+          // transform: "@material-ui/system/esm/${member}",
           // for bundlers not supporting ES modules use:
-          // transform: "@material-ui/system/${member}",
+          transform: "@material-ui/system/${member}",
           preventFullImport: true,
         },
       },
