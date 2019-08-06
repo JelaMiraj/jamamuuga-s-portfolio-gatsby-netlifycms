62b70251588ef9c20c38804d50d3709e225f0192
Jason Cady <jamamuuga@gmail.com>
2019-08-06 16:16:21 -0700

    Auto-format code.

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-  presets: [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "babel-preset-gatsby",
-  ], // .map(require.resolve),
+  presets: ["@babel/preset-env", "@babel/preset-react", "babel-preset-gatsby"], // .map(require.resolve),
   plugins: [
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-syntax-dynamic-import",
--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -1,7 +1,11 @@
 import React from "react"
 import {PropTypes} from "prop-types"
 // import {Helmet} from "react-helmet"
-import {createMuiTheme, CssBaseline, responsiveFontSizes} from "@material-ui/core"
+import {
+  createMuiTheme,
+  CssBaseline,
+  responsiveFontSizes,
+} from "@material-ui/core"
 import {red, pink} from "@material/core/colors"
 import {ThemeProvider} from "@material-ui/styles"
 import {Helmet} from "../../src/components"
@@ -9,11 +13,12 @@ import {Helmet} from "../../src/components"
 
 const theme = responsiveFontSizes(
   createMuiTheme({
-  palette: {
-    primary: red,
-    secondary: pink,
-  },
-}))
+    palette: {
+      primary: red,
+      secondary: pink,
+    },
+  }),
+)
 
 export default function TopLayout(props) {
   return (
