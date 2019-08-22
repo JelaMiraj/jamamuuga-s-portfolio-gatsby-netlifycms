abe5a31e8d7707e666d294330165035583befbdd
Jason Cady <jamamuuga@gmail.com>
2019-08-22 16:00:51 -0700

    Rename theme variable in TopLayout.

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -10,9 +10,9 @@ import {red, pink} from "@material-ui/core/colors"
 // import {ThemeProvider} from "@material-ui/styles"
 import styled, {ThemeProvider} from "styled-components"
 import {Helmet} from "../../src/components"
-// import {theme} from "../../src/styles/themes"
+// import {theme as myTheme} from "../../src/styles/themes"
 
-const theme = responsiveFontSizes(
+const myTheme = responsiveFontSizes(
   createMuiTheme({
     palette: {
       primary: red,
@@ -25,7 +25,7 @@ export default function TopLayout(props) {
   return (
     <>
       <Helmet />
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={myTheme}>
         <div>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
