c46e677a9126b36a3f1dfac2bfb3c4dc2abde5f9
Jason Cady <jamamuuga@gmail.com>
2019-08-29 20:34:35 -0700

    Added comments about future direction in Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,9 @@ import styled from "styled-components"
 import {PaletteOutline} from "mdi-material-ui"
 import {Link, Menu, MenuMobile} from "."
 // FIXME: Get color assignment issues sorted out so things like hard-coding aren't necessary.
+// TODO: Use color: inherit where it works & lines up right.
 
+// TODO: Hopefully explicit props passing isn't really needed.
 // eslint-disable-next-line
 const StyledAppBar = styled(({...other}) => <AppBar {...other} />)`
   /* color: ${props => props.color || "#f44336"}; */
