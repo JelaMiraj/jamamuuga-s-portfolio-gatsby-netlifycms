4a61e130a3227272c2c25e8e47906a5bb54f92bd
Jason Cady <jamamuuga@gmail.com>
2019-09-05 21:07:14 -0700

    Use Link component from Gatsby Theme Material UI.

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,13 @@
-// See https://github.com/mui-org/material-ui/tree/master/examples/gatsby
-import React from "react"
-import {Link as MuiLink} from "@material-ui/core"
-import {Link as GatsbyLink} from "gatsby"
+// // See https://github.com/mui-org/material-ui/tree/master/examples/gatsby
+// import React from "react"
+// import {Link as MuiLink} from "@material-ui/core"
+// import {Link as GatsbyLink} from "gatsby"
+import {Link} from "gatsby-theme-material-ui"
+// TODO: Switch to direct use of Link when a source stabilizes such as Gatsby Theme or Plugin for Material UI. This may mean waiting for an official first party source.
 
-function Link(props) {
-  // eslint-disable-next-line
-  return <MuiLink component={GatsbyLink} {...props} />
-}
+// function Link(props) {
+//   // eslint-disable-next-line
+//   return <MuiLink component={GatsbyLink} {...props} />
+// }
 
 export default Link
