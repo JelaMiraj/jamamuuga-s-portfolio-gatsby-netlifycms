986899d5d099087ac560f0806b72a8d95f74c09b
Jason Cady <jamamuuga@gmail.com>
2019-09-12 18:46:46 -0700

    Fixed typo in theme import in override of Gatsby Theme Material UI Top Layout.

--- src/gatsby-theme-material-ui-top-layout/components/top-layout.js
+++ src/gatsby-theme-material-ui-top-layout/components/top-layout.js
@@ -4,7 +4,7 @@ import {Box, CssBaseline} from "@material-ui/core"
 import {StylesProvider, ThemeProvider} from "@material-ui/styles"
 import styled, {ThemeProvider as StyledThemeProvider} from "styled-components/macro"
 import {Helmet} from "."
-import theme as myTheme from "../theme"
+import myTheme from "../theme"
 // TODO: Avoid the need to wrap multiple providers.
 // TODO: Convert website & local Gatsby plugin into a monorepo using Lerna & Yarn Workspaces to help guard against things like Yarn PnP issues.
 
