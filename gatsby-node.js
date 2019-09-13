3ff33718a636f328dc249969ea6d552ce1b5b2be
Jason Cady <jamamuuga@gmail.com>
2019-09-13 04:29:36 -0700

    comment out broken section in Gatsby Node Webpack config.

--- gatsby-node.js
+++ gatsby-node.js
@@ -98,14 +98,15 @@ exports.onCreateWebpackConfig = ({stage, rules, loaders, plugins, actions}) => {
     resolveLoader: {
       plugins: [PnpWebpackPlugin.moduleLoader(module)],
     },
-    resolve: {
-      alias: {
-        "@material-ui/styles": path.resolve(
-          appFolder,
-          "node_modules",
-          "@material-ui/styles",
-        ),
-      },
+    // TODO: Replace "appFolder" with the real folder name.
+    // resolve: {
+    //   alias: {
+    //     "@material-ui/styles": path.resolve(
+    //       appFolder,
+    //       "node_modules",
+    //       "@material-ui/styles",
+    //     ),
+    //   },
       plugins: [
         PnpWebpackPlugin.bind(`${__dirname}/.cache`, module, `gatsby`),
         PnpWebpackPlugin.bind(`${__dirname}/public`, module, `gatsby`),
