463579a51d0d01aa6c83548b7de99457f799c587
Jason Cady <jamamuuga@gmail.com>
2019-09-18 22:52:31 -0700

    Only use multiline comment style in styles in Header component.
    Stylelint still can't handle single line comments.

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,41 +20,41 @@ import {Link, Menu, MenuMobile} from "."
 // const StyledAppBar = styled(({color, ...other}) => <AppBar {...other} />)`
 // eslint-disable-next-line
 const StyledAppBar = styled(AppBar)`
-  // "#f44336"
+  /* "#f44336" */
   /* background: ${props =>
     props.background || props.theme.palette.primary.main}; */
-  // background: #f44336;
-  // background-color: #f44336;
-  // Text Color
-  // color: #fff;
+  /* background: #f44336; */
+  /* background-color: #f44336; */
+  /* Text Color */
+  /* color: #fff; */
 
-  /* .MuiAppBar-colorPrimary {
-    // TODO: Set color: to get color of textPrimary from passed theme object. */
+  /* .MuiAppBar-colorPrimary { */
+    /* TODO: Set color: to get color of textPrimary from passed theme object. */
     /* background-color: ${props.color || props.theme.palette.primary.main}; */
-    /* background-color: #f44336;
-    // Text Color
-    color: #fff;
-  } */
+    /* background-color: #f44336; */
+    /* Text Color */
+    /* color: #fff; */
+  /* } */
 `
 
 const StyledAvatar = styled(Avatar)`
-  // TODO: Work on accessibility of colors & color combos.
+  /* TODO: Work on accessibility of colors & color combos. */
   /* background-color: ${props =>
     props.backgroundColor || props.theme.palette.secondary.light}; */
   /* background: ${props.theme.palette.secondary.light}; */
-  // TODO: Use indirect "secondary.light" or refer to theme object instead of hard coding color.
+  /* TODO: Use indirect "secondary.light" or refer to theme object instead of hard coding color. */
   background: rgb(255, 102, 154);
-  // background-color: rgb(255, 102, 154);
+  /* background-color: rgb(255, 102, 154); */
   color: white;
 
-  // .MuiAvatar-colorDefault {
-  //   background-color: rgb(255, 102, 154);
-  // }
+  /* .MuiAvatar-colorDefault {
+    background-color: rgb(255, 102, 154);
+  } */
 `
 
 const StyledChip = styled(Chip)`
   /* color: ${props => props.color || props.theme.palette.secondary.light}; */
-  // color: rgb(255, 102, 154);
+  /* color: rgb(255, 102, 154); */
 `
 
 const Header = ({data}) => (
