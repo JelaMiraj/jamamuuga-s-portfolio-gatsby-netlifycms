3e0e2fe45db390ff63b1d51c0f04cb8ff6a93861
Jason Cady <jamamuuga@gmail.com>
2019-10-03 04:42:46 -0700

    Converted content style to styled components.

--- src/styles/components/content-style.js
+++ src/styles/components/content-style.js
@@ -1,26 +1,23 @@
-import {styled} from "@material-ui/core"
+import styled from "styled-components/macro"
 
-const ContentStyle = styled("div")({
-  // const ContentStyle = makesStyles({
-  content: {
-    "& taglist": {
-      "list-style": "none",
-      "margin-bottom": 0,
-      "margin-left": 0,
-      "margin-right": "1.5rem",
-      "margin-top": "1.5rem",
-      display: "flex",
-      "flex-wrap": "wrap",
-      "justify-content": "left",
-      "align-items": "center",
+const ContentStyle = styled.div`
+  & .taglist {
+    list-style: none;
+    margin-bottom: 0;
+    margin-left: 0;
+    margin-right: 1.5rem;
+    margin-top: 1.5rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+    align-items: center;
 
-      "& li": {
-        padding: "0 2rem 1rem 0",
-        "margin-bottom": "1.5rem",
-        "margin-top": 0,
-      },
-    },
-  },
-})
+    & .li {
+      padding: 0 2rem 1rem 0;
+      margin-bottom: 1.5rem;
+      margin-top: 0;
+    }
+  }
+`
 
 export default ContentStyle
